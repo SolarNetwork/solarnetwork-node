9481d9a9cef25d62ef929aeb3ba9e44cf6b52365
Matt Magoffin <git+matt@msqr.us>
2012-12-05 20:28:00 +1300

    Fix setting keys so they all start with "solarnode." and aren't considered by Config Admin setup service.

--- net.solarnetwork.node/src/net/solarnetwork/node/SetupSettings.java
+++ net.solarnetwork.node/src/net/solarnetwork/node/SetupSettings.java
@@ -36,18 +36,18 @@ public final class SetupSettings {
 	public static final String SETUP_TYPE_KEY = "solarnode.setup";
 
 	/** The node ID. */
-	public static final String KEY_NODE_ID = "node.id";
+	public static final String KEY_NODE_ID = "solarnode.id";
 
 	/** The SolarNetwork server host name. */
-	public static final String KEY_SOLARNETWORK_HOST_NAME = "solarnetwork.host";
+	public static final String KEY_SOLARNETWORK_HOST_NAME = "solarnode.solarnet.host";
 
 	/** The SolarNetwork server host port. */
-	public static final String KEY_SOLARNETWORK_HOST_PORT = "solarnetwork.port";
+	public static final String KEY_SOLARNETWORK_HOST_PORT = "solarnode.solarnet.port";
 
 	/** The SolarNetwork force TLS status. */
-	public static final String KEY_SOLARNETWORK_FORCE_TLS = "solarnetwork.forceTLS";
+	public static final String KEY_SOLARNETWORK_FORCE_TLS = "solarnode.solarnet.forceTLS";
 
 	/** The confirmation code supplied by the SolarNetwork server. */
-	public static final String KEY_CONFIRMATION_CODE = "solarnetwork.confirmation";
+	public static final String KEY_CONFIRMATION_CODE = "solarnode.solarnet.confirmation";
 
 }
