9e6498849ff5566ef85327cc9649bde6093f6e75
Matt Magoffin <git+matt@msqr.us>
2013-03-30 11:51:44 +1300

    Implement FileBackupResourceProvider for backing up node bundles, e.g. app/base and app/main directories.

---
+++ net.solarnetwork.node/src/net/solarnetwork/node/Constants.java
@@ -0,0 +1,40 @@
+/* ==================================================================
+ * Constants.java - Mar 30, 2013 11:07:00 AM
+ * 
+ * Copyright 2007-2013 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node;
+
+/**
+ * SolarNode constants.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public final class Constants {
+
+	/** The system property for the node's home directory. */
+	public static final String SYSTEM_PROP_NODE_HOME = "sn.home";
+
+	private Constants() {
+		// don't construct me
+	}
+
+}
---
+++ net.solarnetwork.node/src/net/solarnetwork/node/backup/FileBackupResourceProvider.java
@@ -0,0 +1,184 @@
+/* ==================================================================
+ * FileBackupResourceProvider.java - Mar 28, 2013 8:42:37 PM
+ * 
+ * Copyright 2007-2013 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.backup;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import net.solarnetwork.node.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * {@link BackupResourceProvider} for node files, such as installed application
+ * bundle JARs.
+ * 
+ * <p>
+ * The configurable properties of this class are:
+ * </p>
+ * 
+ * <dl class="class-properties">
+ * <dt>rootPath</dt>
+ * <dd>The root path from which {@code resourceDirectories} are relative to. If
+ * not provided, the runtime working directory will be used.</dd>
+ * <dt>resourceDirectories</dt>
+ * <dd>An array of directory paths, relative to {@code rootPath}, to look for
+ * files to include in the backup. Defaults to {@code [app/base, app/main]}.</dd>
+ * 
+ * <dt>fileNamePattern</dt>
+ * <dd>A regexp used to match against files found in the
+ * {@code resourceDirectories}. Only files matching this expression are included
+ * in the backup. Defaults to {@code \.jar$}.</dd>
+ * </dl>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class FileBackupResourceProvider implements BackupResourceProvider {
+
+	private String rootPath = System.getProperty(Constants.SYSTEM_PROP_NODE_HOME, "");
+	private String[] resourceDirectories = new String[] { "app/base", "app/main" };
+	private String fileNamePattern = "\\.jar$";
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public String getKey() {
+		return FileBackupResourceProvider.class.getName();
+	}
+
+	@Override
+	public Iterable<BackupResource> getBackupResources() {
+		if ( resourceDirectories == null || resourceDirectories.length < 1 ) {
+			return Collections.emptyList();
+		}
+		final Pattern pat = (fileNamePattern == null ? null : Pattern.compile(fileNamePattern,
+				Pattern.CASE_INSENSITIVE));
+		List<BackupResource> fileList = new ArrayList<BackupResource>(20);
+		for ( String path : resourceDirectories ) {
+			File rootDir = (rootPath != null && rootPath.length() > 0 ? new File(rootPath, path)
+					: new File(path));
+			if ( !rootDir.isDirectory() ) {
+				log.warn("Skipping path {} because does not exist or is not a directory",
+						rootDir.getAbsolutePath());
+				continue;
+			}
+			File[] files = rootDir.listFiles(new FilenameFilter() {
+
+				@Override
+				public boolean accept(File dir, String name) {
+					return pat.matcher(name).find();
+				}
+			});
+			if ( files == null || files.length < 1 ) {
+				continue;
+			}
+			for ( File f : files ) {
+				// make sure backup path is relative
+				final String backupPath = path + '/' + f.getName();
+				fileList.add(new ResourceBackupResource(new FileSystemResource(f), backupPath));
+			}
+		}
+		return fileList;
+	}
+
+	@Override
+	public boolean restoreBackupResource(BackupResource resource) {
+		if ( resourceDirectories != null && resourceDirectories.length > 0 ) {
+			for ( String path : resourceDirectories ) {
+				File rootDir = new File(rootPath, path);
+				File backupFile = new File(resource.getBackupPath());
+				if ( backupFile.getAbsolutePath().startsWith(rootDir.getAbsolutePath()) ) {
+					if ( !backupFile.getParentFile().isDirectory()
+							&& !backupFile.getParentFile().mkdirs() ) {
+						log.warn("Unable to create directory {} to restore file {} into",
+								backupFile.getParent(), resource.getBackupPath());
+					}
+					try {
+						FileCopyUtils.copy(resource.getInputStream(), new FileOutputStream(backupFile));
+						backupFile.setLastModified(resource.getModificationDate());
+					} catch ( IOException e ) {
+						log.error("Unable to restore backup resource {} to {}: {}",
+								resource.getBackupPath(), rootDir.getPath(), e.getMessage());
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Set the {@code resourceDirectories} property as a comma-delimieted
+	 * string.
+	 * 
+	 * @param list
+	 *        a comma-delimited list of paths
+	 */
+	public void setResourceDirs(String list) {
+		setResourceDirectories(StringUtils.commaDelimitedListToStringArray(list));
+	}
+
+	/**
+	 * Get a comma-delimited string of the configured
+	 * {@code resourceDirectories} property.
+	 * 
+	 * @return a comma-delimited list of paths
+	 */
+	public String getResourceDirs() {
+		return StringUtils.arrayToCommaDelimitedString(getResourceDirectories());
+	}
+
+	public void setRootPath(String rootPath) {
+		this.rootPath = rootPath;
+	}
+
+	public void setResourceDirectories(String[] bundlePaths) {
+		this.resourceDirectories = bundlePaths;
+	}
+
+	public void setFileNamePattern(String fileNamePattern) {
+		this.fileNamePattern = fileNamePattern;
+	}
+
+	public String getRootPath() {
+		return rootPath;
+	}
+
+	public String[] getResourceDirectories() {
+		return resourceDirectories;
+	}
+
+	public String getFileNamePattern() {
+		return fileNamePattern;
+	}
+
+}
