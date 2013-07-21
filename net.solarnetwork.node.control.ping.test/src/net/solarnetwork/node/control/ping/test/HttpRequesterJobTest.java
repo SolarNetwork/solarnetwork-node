ca39d081ba2c9f8ae804cf346cc24f244097876a
Matt Magoffin <git+matt@msqr.us>
2013-07-21 12:34:26 +1200

    Use internal HTTP server so don't rely on external resource for test.

--- net.solarnetwork.node.control.ping.test/src/net/solarnetwork/node/control/ping/test/HttpRequesterJobTest.java
+++ net.solarnetwork.node.control.ping.test/src/net/solarnetwork/node/control/ping/test/HttpRequesterJobTest.java
@@ -26,7 +26,11 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
+import java.io.IOException;
 import java.util.Collections;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import net.solarnetwork.node.control.ping.HttpRequesterJob;
 import net.solarnetwork.node.reactor.InstructionHandler;
 import net.solarnetwork.node.reactor.InstructionStatus.InstructionState;
@@ -35,8 +39,12 @@ import net.solarnetwork.node.test.AbstractNodeTest;
 import org.easymock.Capture;
 import org.easymock.CaptureType;
 import org.easymock.EasyMock;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
 import org.quartz.JobExecutionException;
 
 /**
@@ -49,24 +57,71 @@ public class HttpRequesterJobTest extends AbstractNodeTest {
 
 	private static final String TEST_CONTROL_ID = "/test/toggle";
 
-	public InstructionHandler handler;
+	private InstructionHandler handler;
+	private Server server;
+
+	private class TestHandler extends AbstractHandler {
+
+		private int count;
+
+		private TestHandler() {
+			super();
+			count = 0;
+		}
+
+		@Override
+		public void handle(String target, HttpServletRequest request, HttpServletResponse response,
+				int dispatch) throws IOException, ServletException {
+			log.debug("Request query: {}", request.getQueryString());
+			assertEquals("HEAD", request.getMethod());
+			response.setStatus(HttpServletResponse.SC_OK);
+			((Request) request).setHandled(true);
+			count++;
+		}
+
+		private int getCount() {
+			return count;
+		}
+	}
 
 	private HttpRequesterJob newJobInstance() {
 		HttpRequesterJob job = new HttpRequesterJob();
 		job.setControlId(TEST_CONTROL_ID);
-		job.setUrl("http://www.google.com/");
+		job.setUrl("http://localhost:8988/");
 		job.setHandlers(Collections.singletonList(handler));
 		job.setSleepSeconds(0);
 		return job;
 	}
 
 	@Before
-	public void setup() {
+	public void setup() throws Exception {
 		handler = EasyMock.createMock(InstructionHandler.class);
+		server = new Server(8988);
+		server.start();
+	}
+
+	@After
+	public void teardown() throws Exception {
+		if ( server != null && server.isRunning() ) {
+			server.stop();
+		}
 	}
 
 	@Test
-	public void pingGoogle() throws JobExecutionException {
+	public void pingSuccessful() throws JobExecutionException {
+		final TestHandler httpHandler = new TestHandler();
+		server.setHandler(httpHandler);
+		replay(handler);
+		HttpRequesterJob job = newJobInstance();
+		job.execute(null);
+		verify(handler);
+		assertEquals(1, httpHandler.getCount());
+	}
+
+	@Test
+	public void pingFailure() throws Exception {
+		final TestHandler httpHandler = new TestHandler();
+		server.stop();
 		Capture<BasicInstruction> instructions = new Capture<BasicInstruction>(CaptureType.ALL);
 		expect(handler.handlesTopic(InstructionHandler.TOPIC_SET_CONTROL_PARAMETER)).andReturn(
 				Boolean.TRUE);
@@ -80,11 +135,11 @@ public class HttpRequesterJobTest extends AbstractNodeTest {
 		HttpRequesterJob job = newJobInstance();
 		job.execute(null);
 		verify(handler);
+		assertEquals(0, httpHandler.getCount());
 		assertEquals(2, instructions.getValues().size());
 		BasicInstruction instr1 = instructions.getValues().get(0);
 		assertEquals(Boolean.FALSE.toString(), instr1.getParameterValue(TEST_CONTROL_ID));
 		BasicInstruction instr2 = instructions.getValues().get(1);
 		assertEquals(Boolean.TRUE.toString(), instr2.getParameterValue(TEST_CONTROL_ID));
 	}
-
 }
