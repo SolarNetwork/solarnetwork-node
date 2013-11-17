6694f21566eddcd598ad86ea9b98b9ce20f3ed26
Matt Magoffin <git+matt@msqr.us>
2013-11-17 16:28:27 +1300

    Remove duplicates before change primary key.

--- net.solarnetwork.node.consumption.dao.jdbc/src/net/solarnetwork/node/dao/jdbc/consumption/derby-consumptiondatum-update-8.sql
+++ net.solarnetwork.node.consumption.dao.jdbc/src/net/solarnetwork/node/dao/jdbc/consumption/derby-consumptiondatum-update-8.sql
@@ -1,6 +1,13 @@
 DROP TABLE solarnode.sn_consum_datum_upload;
 DELETE FROM solarnode.sn_settings WHERE skey = 'solarnode.sn_consum_datum_upload.version';
 
+DELETE FROM solarnode.sn_consum_datum WHERE id IN (
+	SELECT p.id FROM solarnode.sn_consum_datum p 
+	INNER JOIN (
+		SELECT created, source_id, MIN(id) as id FROM solarnode.sn_consum_datum GROUP BY created, source_id HAVING count(created) > 1
+	) s ON p.created = s.created and p.source_id = s.source_id and p.id <> s.id
+	WHERE p.created = s.created and p.source_id = s.source_id and p.id <> s.id);
+
 ALTER TABLE solarnode.sn_consum_datum ADD COLUMN uploaded TIMESTAMP;
 ALTER TABLE solarnode.sn_consum_datum DROP COLUMN id;
 ALTER TABLE solarnode.sn_consum_datum ADD PRIMARY KEY (created, source_id);
