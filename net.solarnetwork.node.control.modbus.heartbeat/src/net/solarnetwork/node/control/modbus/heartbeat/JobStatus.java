d791c6c499202262c3a8b66ca244cee5f8ce208e
Matt Magoffin <git+matt@msqr.us>
2014-03-22 20:34:43 +1300

    Add a simple "heartbeat" job that can serve to notify some other device that SolarNode is "alive and well".

---
+++ net.solarnetwork.node.control.modbus.heartbeat/src/net/solarnetwork/node/control/modbus/heartbeat/JobStatus.java
@@ -0,0 +1,87 @@
+/* ==================================================================
+ * JobStatus.java - Mar 22, 2014 8:09:12 PM
+ * 
+ * Copyright 2007-2014 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.control.modbus.heartbeat;
+
+import org.joda.time.DateTime;
+
+/**
+ * Heartbeat status info bean.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class JobStatus {
+
+	private final DateTime date;
+	private final boolean successful;
+	private final String message;
+
+	/**
+	 * Construct with values.
+	 * 
+	 * @param date
+	 *        the date the heartbeat was executed
+	 * @param successful
+	 *        <em>true</em> if the heartbeat was executed successfully
+	 * @param message
+	 *        a message
+	 */
+	public JobStatus(DateTime heartbeatDate, boolean successful, String heartbeatMessage) {
+		super();
+		this.date = heartbeatDate;
+		this.successful = successful;
+		this.message = heartbeatMessage;
+	}
+
+	public DateTime getDate() {
+		return date;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public boolean isSuccessful() {
+		return successful;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("JobStatus{");
+		if ( date != null ) {
+			builder.append("date=");
+			builder.append(date);
+			builder.append(", ");
+		}
+		builder.append("successful=");
+		builder.append(successful);
+		if ( message != null ) {
+			builder.append(", message=");
+			builder.append(message);
+		}
+		builder.append("}");
+		return builder.toString();
+	}
+
+}
---
+++ net.solarnetwork.node.control.modbus.heartbeat/src/net/solarnetwork/node/control/modbus/heartbeat/ModbusHeartbeatJob.java
@@ -0,0 +1,198 @@
+/* ==================================================================
+ * ModbusHeartbeatJob.java - Mar 22, 2014 4:02:14 PM
+ * 
+ * Copyright 2007-2014 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.control.modbus.heartbeat;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import net.solarnetwork.node.io.modbus.ModbusConnectionCallback;
+import net.solarnetwork.node.io.modbus.ModbusHelper;
+import net.solarnetwork.node.io.modbus.ModbusSerialConnectionFactory;
+import net.solarnetwork.node.job.AbstractJob;
+import net.solarnetwork.node.settings.SettingSpecifier;
+import net.solarnetwork.node.settings.SettingSpecifierProvider;
+import net.solarnetwork.node.settings.support.BasicTextFieldSettingSpecifier;
+import net.solarnetwork.node.settings.support.BasicTitleSettingSpecifier;
+import net.solarnetwork.node.settings.support.BasicToggleSettingSpecifier;
+import net.solarnetwork.util.DynamicServiceTracker;
+import net.wimpi.modbus.net.SerialConnection;
+import org.joda.time.DateTime;
+import org.quartz.JobExecutionContext;
+import org.quartz.StatefulJob;
+import org.springframework.context.MessageSource;
+
+/**
+ * Periodically set a Modbus "coil" type register to a specific value, to act as
+ * a "heartbeat" to the device so it knows the SolarNode is alive and well.
+ * 
+ * <p>
+ * The configurable properties of this class are:
+ * </p>
+ * 
+ * <dl class="class-properties">
+ * <dt>address</dt>
+ * <dd>The Modbus address of the coil-type register to use.</dd>
+ * <dt>unitId</dt>
+ * <dd>The Modbus unit ID to use.</dd>
+ * <dt>registerValue</dt>
+ * <dd>The value to set the Modbus register to.</dd>
+ * <dt>connectionFactory</dt>
+ * <dd>The {@link ModbusSerialConnectionFactory} to use.</dd>
+ * <dt>messageSource</dt>
+ * <dd>The {@link MessageSource} to use to support
+ * {@link SettingSpecifierProvider}.</dd>
+ * </dl>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class ModbusHeartbeatJob extends AbstractJob implements StatefulJob, SettingSpecifierProvider {
+
+	private Integer address = 0x4008;
+	private Integer unitId = 1;
+	private Boolean registerValue = Boolean.TRUE;
+	private DynamicServiceTracker<ModbusSerialConnectionFactory> connectionFactory;
+	private MessageSource messageSource;
+
+	// static map to keep track of job execution status info
+	private static final ConcurrentMap<Long, JobStatus> STATUS_MAP = new ConcurrentHashMap<Long, JobStatus>(
+			8);
+
+	@Override
+	protected void executeInternal(JobExecutionContext jobContext) throws Exception {
+		final DateTime heartbeatDate = new DateTime();
+		String heartbeatMessage = null;
+		boolean heartbeatSuccess = false;
+		try {
+			final Boolean executed = setValue(registerValue);
+			if ( executed == null ) {
+				// hmm, how can this be localized?
+				heartbeatMessage = "No Modbus connection available.";
+			} else if ( executed.booleanValue() == false ) {
+				heartbeatMessage = "Unknown Modbus error.";
+			} else {
+				// good
+				heartbeatSuccess = true;
+			}
+		} catch ( RuntimeException e ) {
+			log.error("Error sending heartbeat message: {}", e);
+			Throwable root = e;
+			while ( root.getCause() != null ) {
+				root = root.getCause();
+			}
+			heartbeatMessage = "Error: " + root.getMessage();
+		}
+		STATUS_MAP.put(getStatusKey(), new JobStatus(heartbeatDate, heartbeatSuccess, heartbeatMessage));
+	}
+
+	private Long getStatusKey() {
+		return ((unitId == null ? 0L : unitId.longValue()) << 32)
+				| (address == null ? 0L : address.longValue());
+	}
+
+	private synchronized Boolean setValue(Boolean desiredValue) {
+		final BitSet bits = new BitSet(1);
+		bits.set(0, desiredValue);
+		log.info("Setting modbus unit {} register {} value to {}", unitId, address, desiredValue);
+		final Integer[] addresses = new Integer[] { address };
+		return ModbusHelper.execute(connectionFactory, new ModbusConnectionCallback<Boolean>() {
+
+			@Override
+			public Boolean doInConnection(SerialConnection conn) throws IOException {
+				return ModbusHelper.writeDiscreetValues(conn, addresses, bits, unitId);
+			}
+
+		});
+	}
+
+	// SettingSpecifierProvider
+
+	@Override
+	public String getSettingUID() {
+		return "net.solarnetwork.node.control.modbus.heartbeat";
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "Modbus Heartbeat";
+	}
+
+	@Override
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	@Override
+	public List<SettingSpecifier> getSettingSpecifiers() {
+		ModbusHeartbeatJob defaults = new ModbusHeartbeatJob();
+		List<SettingSpecifier> results = new ArrayList<SettingSpecifier>(6);
+
+		// add info on last execution state
+		BasicTitleSettingSpecifier lhDate = new BasicTitleSettingSpecifier("lastHeartbeatDate", "N/A",
+				true);
+
+		JobStatus lastHeartbeatStatus = STATUS_MAP.get(getStatusKey());
+		if ( lastHeartbeatStatus != null ) {
+			lhDate.setDefaultValue(lastHeartbeatStatus.getDate().toString());
+		}
+		results.add(lhDate);
+		if ( lastHeartbeatStatus != null && lastHeartbeatStatus.getMessage() != null ) {
+			results.add(new BasicTitleSettingSpecifier("lastHeartbeatMessage", lastHeartbeatStatus
+					.getMessage(), true));
+		}
+
+		results.add(new BasicTextFieldSettingSpecifier("connectionFactory.propertyFilters['UID']",
+				"/dev/ttyUSB0"));
+		results.add(new BasicTextFieldSettingSpecifier("unitId", defaults.unitId.toString()));
+		results.add(new BasicTextFieldSettingSpecifier("address", defaults.address.toString()));
+		results.add(new BasicToggleSettingSpecifier("registerValue", defaults.registerValue.toString()));
+		return results;
+	}
+
+	// Accessors
+
+	public void setAddress(Integer address) {
+		this.address = address;
+	}
+
+	public void setUnitId(Integer unitId) {
+		this.unitId = unitId;
+	}
+
+	public void setConnectionFactory(
+			DynamicServiceTracker<ModbusSerialConnectionFactory> connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
+
+	public void setRegisterValue(Boolean registerValue) {
+		this.registerValue = registerValue;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+}
