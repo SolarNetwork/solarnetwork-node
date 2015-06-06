d1a8a2299c02bd99ff024addccf77926c4ea97e0
Matt Magoffin <git+matt@msqr.us>
2015-06-06 19:04:26 +1200

    Update IdentityService to provide the configured node Principal, e.g. X500 subjectDN value. This will support passing this as an identifier to other services, such as OCPP.

--- net.solarnetwork.node/src/net/solarnetwork/node/IdentityService.java
+++ net.solarnetwork.node/src/net/solarnetwork/node/IdentityService.java
@@ -24,11 +24,13 @@
 
 package net.solarnetwork.node;
 
+import java.security.Principal;
+
 /**
  * API for knowing information about the node's identity.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public interface IdentityService {
 
@@ -38,6 +40,13 @@ public interface IdentityService {
 	 * @return node ID, or <em>null</em> if the ID is not known
 	 */
 	Long getNodeId();
+
+	/**
+	 * Get a {@link Principal} for the current node.
+	 * 
+	 * @return The node Principal, or <em>null</em> if none available.
+	 */
+	Principal getNodePrincipal();
 
 	/**
 	 * Get the host name for the SolarNet central service.
