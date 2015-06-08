b54ca6bd86e4426d95571bb28ec1c78a3901ea51
Matt Magoffin <git+matt@msqr.us>
2015-06-09 10:56:56 +1200

    Start work on ChargeSessionManager API.

--- net.solarnetwork.node.ocpp/src/net/solarnetwork/node/ocpp/ChargeSessionManager.java
+++ net.solarnetwork.node.ocpp/src/net/solarnetwork/node/ocpp/ChargeSessionManager.java
@@ -22,22 +22,43 @@
 
 package net.solarnetwork.node.ocpp;
 
-
 /**
- * FIXME
- * 
- * <p>TODO</p>
- * 
- * <p>The configurable properties of this class are:</p>
- * 
- * <dl class="class-properties">
- *   <dt></dt>
- *   <dd></dd>
- * </dl>
+ * API for managing charge sessions. A <em>charge session</em> is the process of
+ * charging an electric device, e.g. plugging the device into a socket,
+ * authorizing the use of the socket with the OCPP central system, monitoring
+ * the energy used while charging, and unplugging the device from the socket,
+ * and finally confirming that charging is complete.
  * 
  * @author matt
  * @version 1.0
  */
 public interface ChargeSessionManager {
+
+	/**
+	 * Initiate a new charge session.
+	 * 
+	 * @param idTag
+	 *        The ID to request authorization with.
+	 * @param socketId
+	 *        The ID of the physical socket to enable
+	 * @param reservationId
+	 *        An optional reservation ID. The reservation will be considered
+	 *        satisfied upon successful return from this method.
+	 * @return A unique charge session ID.
+	 */
+	String initiateChargeSession(String idTag, String socketId, String reservationId);
+
+	/**
+	 * Complete a charge session. This method must be called at some point after
+	 * {@link #initiateChargeSession(String, String, String)}, passing in the
+	 * return value of that method as the {@code sessionId}.
+	 * 
+	 * @param idTag
+	 *        The ID to request authorization with.
+	 * @param sessionId
+	 *        The session ID returned from a previous call to
+	 *        {@link #initiateChargeSession(String, String, String)}.
+	 */
+	void completeChargeSession(String idTag, String sessionId);
 
 }
