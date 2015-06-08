41d883b35e914be354af7bcc7618fffd7c8c88f5
Matt Magoffin <git+matt@msqr.us>
2015-06-08 21:02:20 +1200

    NODE-64: add periodic job to delete expired authorizations from the local database.

---
+++ net.solarnetwork.node.ocpp/src/net/solarnetwork/node/ocpp/ChargeSessionManager.java
@@ -0,0 +1,43 @@
+/* ==================================================================
+ * ChargeSessionManager.java - 8/06/2015 7:04:34 am
+ * 
+ * Copyright 2007-2015 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.ocpp;
+
+
+/**
+ * FIXME
+ * 
+ * <p>TODO</p>
+ * 
+ * <p>The configurable properties of this class are:</p>
+ * 
+ * <dl class="class-properties">
+ *   <dt></dt>
+ *   <dd></dd>
+ * </dl>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public interface ChargeSessionManager {
+
+}
