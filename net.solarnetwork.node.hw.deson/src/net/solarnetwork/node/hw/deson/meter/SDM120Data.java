2b6cfc91fca53546fbfa641a7c90c3db4023dfdf
Matt Magoffin <git+matt@msqr.us>
2016-01-25 20:52:33 +1300

    Define SDMData API and create common base class for implementations to extend.

--- net.solarnetwork.node.hw.deson/src/net/solarnetwork/node/hw/deson/meter/SDM120Data.java
+++ net.solarnetwork.node.hw.deson/src/net/solarnetwork/node/hw/deson/meter/SDM120Data.java
@@ -22,11 +22,7 @@
 
 package net.solarnetwork.node.hw.deson.meter;
 
-import gnu.trove.map.TIntIntMap;
-import gnu.trove.map.hash.TIntIntHashMap;
-import java.util.Arrays;
 import net.solarnetwork.node.io.modbus.ModbusConnection;
-import net.solarnetwork.node.io.modbus.ModbusHelper;
 
 /**
  * Encapsulates raw Modbus register data from SDM 120 meters.
@@ -34,13 +30,13 @@ import net.solarnetwork.node.io.modbus.ModbusHelper;
  * @author matt
  * @version 1.0
  */
-public class SDM120Data {
-
-	// current (Float32)
-	public static final int ADDR_DATA_I = 6;
+public class SDM120Data extends BaseSDMData {
 
 	// voltage (Float32)
 	public static final int ADDR_DATA_V_NEUTRAL = 0;
+
+	// current (Float32)
+	public static final int ADDR_DATA_I = 6;
 
 	// power (Float32)
 	public static final int ADDR_DATA_ACTIVE_POWER = 12;
@@ -59,15 +55,11 @@ public class SDM120Data {
 	public static final int ADDR_DATA_REACTIVE_ENERGY_IMPORT_TOTAL = 76;
 	public static final int ADDR_DATA_REACTIVE_ENERGY_EXPORT_TOTAL = 78;
 
-	private final TIntIntMap dataRegisters;
-	private long dataTimestamp = 0;
-
 	/**
 	 * Default constructor.
 	 */
 	public SDM120Data() {
 		super();
-		this.dataRegisters = new TIntIntHashMap(64);
 	}
 
 	/**
@@ -77,9 +69,7 @@ public class SDM120Data {
 	 *        the object to copy
 	 */
 	public SDM120Data(SDM120Data other) {
-		super();
-		this.dataRegisters = new TIntIntHashMap(other.dataRegisters);
-		this.dataTimestamp = other.dataTimestamp;
+		super(other);
 	}
 
 	@Override
@@ -94,196 +84,16 @@ public class SDM120Data {
 				+ getEnergy(ADDR_DATA_REACTIVE_ENERGY_EXPORT_TOTAL) + "}";
 	}
 
-	/**
-	 * Get a string of data values, useful for debugging. The generated string
-	 * will contain a register address followed by two register values per line,
-	 * printed as hexidecimal integers, with a prefix and suffix line. The
-	 * register addresses will be printed as {@bold 30001-based} values,
-	 * to match Deson's documentation. For example:
-	 * 
-	 * <pre>
-	 * SDM120Data{
-	 *      30001: 0x4141, 0x727E
-	 *      30007: 0xFFC0, 0x0000
-	 *      ...
-	 *      30345: 0x0000, 0x0000
-	 * }
-	 * </pre>
-	 * 
-	 * @return debug string
-	 */
+	@Override
 	public String dataDebugString() {
-		final StringBuilder buf = new StringBuilder("PM3200Data{\n");
-		SDM120Data snapshot = new SDM120Data(this);
-		int[] keys = snapshot.dataRegisters.keys();
-		Arrays.sort(keys);
-		boolean odd = true;
-		for ( int k : keys ) {
-			if ( odd ) {
-				buf.append("\t").append(String.format("%5d", k + 30001)).append(": ");
-			}
-			buf.append(String.format("0x%04X", snapshot.dataRegisters.get(k)));
-			if ( odd ) {
-				buf.append(", ");
-			} else {
-				buf.append("\n");
-			}
-			odd = !odd;
-		}
-		buf.append("}");
-		return buf.toString();
+		final SDM120Data snapshot = new SDM120Data(this);
+		return dataDebugString(snapshot);
 	}
 
-	/**
-	 * Read data from the meter and store it internally. If data is populated
-	 * successfully, the {@link dataTimestamp} will be updated to the current
-	 * system time. <b>Note</b> this does <b>not</b> call
-	 * {@link #readEnergyRatios(SerialConnection, int)}. Those values are not
-	 * expected to change much, so those values should be called manually as
-	 * needed.
-	 * 
-	 * @param conn
-	 *        the Modbus connection
-	 */
-	public synchronized void readMeterData(final ModbusConnection conn) {
+	@Override
+	public boolean readMeterDataInternal(final ModbusConnection conn) {
 		readIntData(conn, ADDR_DATA_V_NEUTRAL, ADDR_DATA_V_NEUTRAL + 80);
-
-		//		// voltage
-		//		readIntData(conn, ADDR_DATA_V_L1_L2, ADDR_DATA_V_NEUTRAL_AVERAGE + 1);
-		//
-		//		// power, power factor
-		//		readIntData(conn, ADDR_DATA_ACTIVE_POWER_P1, ADDR_DATA_POWER_FACTOR_TOTAL + 1);
-		//
-		//		// tangent phi, frequency, temp (Float32)
-		//		readIntData(conn, ADDR_DATA_REACTIVE_FACTOR_TOTAL, ADDR_DATA_TEMP + 1);
-		//
-		//		// total energy (Int64)
-		//		readIntData(conn, ADDR_DATA_ACTIVE_ENERGY_IMPORT_TOTAL,
-		//				ADDR_DATA_APPARENT_ENERGY_EXPORT_TOTAL + 3);
-		//
-		//		// total phase energy import (Int64)
-		//		readIntData(conn, ADDR_DATA_ACTIVE_ENERGY_IMPORT_P1, ADDR_DATA_APPARENT_ENERGY_IMPORT_P3 + 3);
-
-		dataTimestamp = System.currentTimeMillis();
-	}
-
-	private void readIntData(final ModbusConnection conn, final int startAddr, final int endAddr) {
-		int[] data = conn.readInts(startAddr, (endAddr - startAddr + 1));
-		saveDataArray(data, startAddr);
-	}
-
-	/**
-	 * Internally store an array of 16-bit integer register data values,
-	 * starting at a given address.
-	 * 
-	 * @param data
-	 *        the data array to save
-	 * @param addr
-	 *        the starting address of the data
-	 */
-	protected void saveDataArray(final int[] data, int addr) {
-		if ( data == null || data.length < 1 ) {
-			return;
-		}
-		for ( int v : data ) {
-			dataRegisters.put(addr, v);
-			addr++;
-		}
-	}
-
-	private Float getFloat32(final int addr) {
-		return ModbusHelper.parseFloat32(dataRegisters.get(addr), dataRegisters.get(addr + 1));
-	}
-
-	/**
-	 * Get an effective voltage value in V.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a voltage value
-	 */
-	public Float getVoltage(final int addr) {
-		return getFloat32(addr);
-	}
-
-	/**
-	 * Get an effective current value in A.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a current value
-	 */
-	public Float getCurrent(int addr) {
-		return getFloat32(addr);
-	}
-
-	/**
-	 * Get an effective frequency value in Hz.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a frequency value
-	 */
-	public Float getFrequency(int addr) {
-		return getFloat32(addr);
-	}
-
-	/**
-	 * Get an effective temperature value in C.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a temperature value
-	 */
-	public Float getTemperature(int addr) {
-		return getFloat32(addr);
-	}
-
-	/**
-	 * Get an effective power factor value.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a power factor value
-	 */
-	public Float getPowerFactor(int addr) {
-		return getFloat32(addr);
-	}
-
-	/**
-	 * Get an effective power value in W (active), Var (reactive) or VA
-	 * (apparent). These are rounded from their native floating point
-	 * representations.
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as a power value
-	 */
-	public Integer getPower(int addr) {
-		Float value = getFloat32(addr);
-		if ( value == null ) {
-			return null;
-		}
-		return Integer.valueOf((int) (Math.round(value.doubleValue())));
-	}
-
-	/**
-	 * Get an effective energy value in Wh (real), Varh (reactive).
-	 * 
-	 * @param addr
-	 *        the register address to read
-	 * @return the value interpreted as an energy value
-	 */
-	public Long getEnergy(int addr) {
-		Float value = getFloat32(addr);
-		if ( value == null ) {
-			return null;
-		}
-		return Long.valueOf(Math.round(value.doubleValue() * 1000.0));
-	}
-
-	public long getDataTimestamp() {
-		return dataTimestamp;
+		return true;
 	}
 
 }
---
+++ net.solarnetwork.node.hw.deson/src/net/solarnetwork/node/hw/deson/meter/SDMData.java
@@ -0,0 +1,134 @@
+/* ==================================================================
+ * SDMData.java - 25/01/2016 5:42:42 pm
+ * 
+ * Copyright 2007-2016 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.hw.deson.meter;
+
+import net.solarnetwork.node.io.modbus.ModbusConnection;
+
+/**
+ * Common API for SDM meter data.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public interface SDMData {
+
+	/**
+	 * Get the system time this data was read from the actual device. If never
+	 * read, then return {@code 0}.
+	 * 
+	 * @return the data timestamp
+	 */
+	long getDataTimestamp();
+
+	/**
+	 * Read data from the meter and store it internally. If data is populated
+	 * successfully, the {@link #getDataTimestamp()} will be updated to reflect
+	 * the current system time.
+	 * 
+	 * @param conn
+	 *        the Modbus connection
+	 */
+	void readMeterData(final ModbusConnection conn);
+
+	/**
+	 * Get a string representation of the meter data, for debugging purposes.
+	 * 
+	 * The generated string will contain prefix and suffix lines, with any
+	 * number of lines between that contain a register address followed by two
+	 * register values per line, printed as hexidecimal integers. For example:
+	 * 
+	 * <pre>
+	 * SDMData{
+	 *      30001: 0x4141, 0x727E
+	 *      30007: 0xFFC0, 0x0000
+	 *      ...
+	 *      30345: 0x0000, 0x0000
+	 * }
+	 * </pre>
+	 * 
+	 * @return A debug string.
+	 */
+	String dataDebugString();
+
+	/**
+	 * Get an effective voltage value in V. Only valid after
+	 * {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as a voltage value
+	 */
+	Float getVoltage(int addr);
+
+	/**
+	 * Get an effective current value in A. Only valid after
+	 * {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as a current value
+	 */
+	Float getCurrent(int addr);
+
+	/**
+	 * Get an effective frequency value in Hz. Only valid after
+	 * {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as a frequency value
+	 */
+	Float getFrequency(int addr);
+
+	/**
+	 * Get an effective power factor value. Only valid after
+	 * {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as a power factor value
+	 */
+	Float getPowerFactor(int addr);
+
+	/**
+	 * Get an effective power value in W (active), Var (reactive) or VA
+	 * (apparent). These are rounded from their native floating point
+	 * representations. Only valid after
+	 * {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as a power value
+	 */
+	Integer getPower(int addr);
+
+	/**
+	 * Get an effective energy value in Wh (real), Varh (reactive). Only valid
+	 * after {@link #readMeterData(ModbusConnection)} called at least once.
+	 * 
+	 * @param addr
+	 *        the register address to read
+	 * @return the value interpreted as an energy value
+	 */
+	Long getEnergy(int addr);
+}
