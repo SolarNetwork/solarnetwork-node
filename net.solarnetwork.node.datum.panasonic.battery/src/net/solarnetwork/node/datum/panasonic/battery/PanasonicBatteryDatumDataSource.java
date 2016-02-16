1691de6bd22e05c9aadf2549085d13e977adce27
Matt Magoffin <git+matt@msqr.us>
2016-02-17 09:45:00 +1300

    Add better error messaging and support for querying by device ID.

--- net.solarnetwork.node.datum.panasonic.battery/src/net/solarnetwork/node/datum/panasonic/battery/PanasonicBatteryDatumDataSource.java
+++ net.solarnetwork.node.datum.panasonic.battery/src/net/solarnetwork/node/datum/panasonic/battery/PanasonicBatteryDatumDataSource.java
@@ -23,9 +23,11 @@
 package net.solarnetwork.node.datum.panasonic.battery;
 
 import java.util.List;
+import java.util.Locale;
 import net.solarnetwork.node.DatumDataSource;
 import net.solarnetwork.node.domain.EnergyStorageDatum;
 import net.solarnetwork.node.domain.GeneralNodeEnergyStorageDatum;
+import net.solarnetwork.node.hw.panasonic.battery.BatteryAPIException;
 import net.solarnetwork.node.hw.panasonic.battery.BatteryAPISupport;
 import net.solarnetwork.node.hw.panasonic.battery.BatteryData;
 import net.solarnetwork.node.settings.SettingSpecifier;
@@ -58,21 +60,60 @@ public class PanasonicBatteryDatumDataSource extends BatteryAPISupport implement
 		DatumDataSource<GeneralNodeEnergyStorageDatum>, SettingSpecifierProvider {
 
 	private String email;
+	private String deviceID;
 	private MessageSource messageSource;
 	private long sampleCacheMs = 5000;
 	private String sourceId = "Battery";
 
-	private BatteryData getCurrentSample() {
-		String e = getEmail();
-		if ( e == null || e.length() < 1 ) {
+	private synchronized BatteryData getCurrentSample() {
+		final String user = getEmail();
+		final String id = getDeviceID();
+		final boolean useDeviceID = (id != null && id.length() > 0);
+		if ( (user == null || user.length() < 1) && !useDeviceID ) {
+			log.info("No device ID or email configured, cannot collect sample.");
 			return null;
 		}
-		BatteryData currSample;
+		BatteryData currSample = sample;
 		if ( isCachedSampleExpired() ) {
-			currSample = getClient().getCurrentBatteryDataForEmail(e);
+			try {
+				if ( useDeviceID ) {
+					currSample = getClient().getCurrentBatteryDataForDevice(id);
+				} else {
+					currSample = getClient().getCurrentBatteryDataForEmail(user);
+				}
+				setErrorMessage(null);
+				sample = currSample;
+			} catch ( BatteryAPIException e ) {
+				String msg = null;
+				if ( e.getCode() > 0 ) {
+					switch (e.getCode()) {
+						case 404:
+							if ( useDeviceID ) {
+								msg = getMessageSource().getMessage("error.code.404.device",
+										new Object[] { id }, Locale.getDefault());
+							} else {
+								msg = getMessageSource().getMessage("error.code.404.email",
+										new Object[] { user }, Locale.getDefault());
+							}
+							break;
+
+						default:
+							msg = getMessageSource().getMessage("error.code",
+									new Object[] { e.getCode() }, Locale.getDefault());
+							break;
+					}
+				} else {
+					msg = "Error communicating with Battery API: " + e.getMessage();
+				}
+				setErrorMessage(msg);
+				if ( e.getCode() > 0 ) {
+					log.warn("Battery API server returned error code {}", e.getCode());
+					return null;
+				} else {
+					throw e;
+				}
+			}
 			log.debug("Read BatteryData sample: {}", currSample);
-		} else {
-			currSample = sample;
 		}
 		return currSample;
 	}
@@ -98,6 +139,9 @@ public class PanasonicBatteryDatumDataSource extends BatteryAPISupport implement
 	public GeneralNodeEnergyStorageDatum readCurrentDatum() {
 		final long start = System.currentTimeMillis();
 		final BatteryData currSample = getCurrentSample();
+		if ( currSample == null ) {
+			return null;
+		}
 		PanasonicBatteryDatum d = new PanasonicBatteryDatum(currSample);
 		d.setSourceId(getSourceId());
 		if ( currSample.getDate() != null && currSample.getDate().getMillis() >= start ) {
@@ -132,12 +176,24 @@ public class PanasonicBatteryDatumDataSource extends BatteryAPISupport implement
 	public List<SettingSpecifier> getSettingSpecifiers() {
 		PanasonicBatteryDatumDataSource defaults = new PanasonicBatteryDatumDataSource();
 		List<SettingSpecifier> results = super.getSettingSpecifiers();
+		results.add(new BasicTextFieldSettingSpecifier("deviceID", ""));
 		results.add(new BasicTextFieldSettingSpecifier("email", ""));
 		results.add(new BasicTextFieldSettingSpecifier("sourceId",
 				String.valueOf(defaults.getSourceId())));
 		results.add(new BasicTextFieldSettingSpecifier("sampleCacheMs", String.valueOf(defaults
 				.getSampleCacheMs())));
 		return results;
+	}
+
+	@Override
+	public String toString() {
+		String key = "deviceID";
+		String ident = getDeviceID();
+		if ( ident == null || ident.length() < 1 ) {
+			key = "email";
+			ident = getEmail();
+		}
+		return getClass().getSimpleName() + "{" + key + "=" + (ident != null ? ident : "") + "}";
 	}
 
 	/**
@@ -177,6 +233,27 @@ public class PanasonicBatteryDatumDataSource extends BatteryAPISupport implement
 	 */
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	/**
+	 * Get the battery device ID to use with the Battery API. Either this or
+	 * {@link #getEmail()} must be configured, with this value being used in
+	 * preference to the other.
+	 * 
+	 * @return The configured battery device ID.
+	 */
+	public String getDeviceID() {
+		return deviceID;
+	}
+
+	/**
+	 * Set the battery device ID to use with the Battery API.
+	 * 
+	 * @param deviceID
+	 *        The battery device ID to use.
+	 */
+	public void setDeviceID(String deviceID) {
+		this.deviceID = deviceID;
 	}
 
 	/**
