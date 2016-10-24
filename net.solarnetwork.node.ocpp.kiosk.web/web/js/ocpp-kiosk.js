e7e51ff04060183b75b5684e6254f6bda48dcca0
Matt Magoffin <matt@solarnetwork.net>
2016-10-25 12:48:09 +1300

    Flush out display/layout of state information.

--- net.solarnetwork.node.ocpp.kiosk.web/web/js/ocpp-kiosk.js
+++ net.solarnetwork.node.ocpp.kiosk.web/web/js/ocpp-kiosk.js
@@ -76,6 +76,24 @@ var OCPPKiosk = (function() {
 		return k.toFixed(1);
 	}
 	
+	function formatEnergy(energy) {
+		return formatPower(energy);
+	}
+		
+	function formatDuration(ms, unitEl) {
+		var mins = (Number.isFinite(ms) ? ms : 0) / 60000;
+		if ( mins > 59.5 ) {
+			if ( unitEl ) {
+				unitEl.innerText = 'hr';
+			}
+			return (mins / 60).toFixed(1);
+		}
+		if ( unitEl ) {
+			unitEl.innerText = 'min';
+		}
+		return mins.toFixed(0);
+	}
+	
 	function refreshPvData(data) {
 		var el = $('#pv-power');
 		if ( el ) {
@@ -83,11 +101,90 @@ var OCPPKiosk = (function() {
 		}
 	}
 	
+	function refreshSocketData(data, key) {
+		var cssKey = key.toLowerCase(),
+			statusEl = $('#socket-status-' +cssKey),
+			powerEl = $('#socket-power-' +cssKey),
+			energyEl = $('#socket-energy-' +cssKey),
+			durEl = $('#socket-duration-' +cssKey),
+			durUnitEl = $('#socket-duration-unit-' +cssKey);
+		statusEl.classList.toggle('active', data.endDate === 0);
+		if ( powerEl ) {
+			powerEl.innerText = formatPower(data.power);
+		}
+		if ( energyEl ) {
+			energyEl.innerText = formatEnergy(data.energy);
+		}
+		if ( durEl ) {
+			durEl.innerText = formatDuration(data.duration, durUnitEl);
+		}
+	}
+	
+	function refreshGridData(data) {
+		var el = $('#grid-power');
+		if ( el ) {
+			el.innerText = formatPower(Math.abs(data.power));
+		}
+	}
+	
 	function processKioskDataUpdate(data) {
 		console.log('Got kisok data: %s', JSON.stringify(data));
+		var key, 
+			pvProd = 0,
+			gridDraw = 0,
+			socketTotalDraw = null,
+			socketDraw = { A:null, B:null },
+			gif = $('#ocpp-state'),
+			gifName;
 		if ( data.pvData ) {
 			refreshPvData(data.pvData);
+			if ( data.pvData.power ) {
+				pvProd = data.pvData.power;
+			}
 		}
+		if ( data.socketData ) {
+			for ( key in data.socketData ) {
+				refreshSocketData(data.socketData[key], key);
+				if ( data.socketData[key].power !== undefined ) {
+					socketDraw[key] = data.socketData[key].power;
+					socketTotalDraw += socketDraw[key];
+				}
+			}
+			
+			// turn off any socket we didn't get data for
+			for ( key in socketDraw ) {
+				if ( socketDraw[key] === null ) {
+					refreshSocketData({}, key)
+				}
+			}
+		}
+		gridDraw = (socketTotalDraw - pvProd);
+		refreshGridData({power:gridDraw});
+		if ( pvProd < 1 && socketTotalDraw === null ) {
+			// nothing going on
+			gifName = 'Off';
+		} else if ( pvProd > 0 && socketTotalDraw === null ) {
+			gifName = 'Export-Only';
+		} else {
+			if ( socketDraw.A !== null && socketDraw.B !== null ) {
+				gifName = 'AB';
+			} else if ( socketDraw.A !== null ) {
+				gifName = 'A';
+			} else {
+				gifName = 'B';
+			}
+			if ( pvProd > 0 && gridDraw > 0 ) {
+				gifName += '1';
+			} else if ( pvProd > 0 && gridDraw === 0 ) {
+				gifName += '2';
+			} else if ( pvProd < 1 && gridDraw > 0 ) {
+				gifName += '3';
+			} else {
+				// exporting
+				gifName += '4';
+			}
+		}
+		gif.src = ('img/'+gifName+'.gif');
 	}
 
 	function websocketConnect() {
@@ -120,7 +217,7 @@ var OCPPKiosk = (function() {
 	}
 	
 	function mockCycle() {
-		var max = 1, 
+		var max = 8, 
 			digits = 2,
 			r = (Math.floor(Math.random() * max) +1),
 			path = ('/data/mock-'+(function() {
@@ -132,9 +229,9 @@ var OCPPKiosk = (function() {
 			}()) + '.json');
 		reqJSON({path : path}).then(function(data) {
 			processKioskDataUpdate(data)
-			//setTimeout(mockCycle, 2000);
+			setTimeout(mockCycle, 2000);
 		}, function(error) {
-			//setTimeout(mockCycle, 2000);
+			setTimeout(mockCycle, 2000);
 		});
 	}
 	
