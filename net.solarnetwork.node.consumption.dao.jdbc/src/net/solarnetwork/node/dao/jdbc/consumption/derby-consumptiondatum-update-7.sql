56df6776eacd0aed4fc223595dae2ae880a8eeb8
Matt Magoffin <matt@solarnetwork.net>
2017-09-26 10:37:08 +1300

    Remove no longer supported legacy Consumption/PowerDatum DAO implementations.

--- net.solarnetwork.node.consumption.dao.jdbc/src/net/solarnetwork/node/dao/jdbc/consumption/derby-consumptiondatum-update-7.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE solarnode.sn_consum_datum
-DROP COLUMN error_msg;
-
-UPDATE solarnode.sn_settings SET svalue = '7' WHERE skey = 'solarnode.sn_consum_datum.version'; 
--- net.solarnetwork.node.consumption.dao.jdbc/src/net/solarnetwork/node/dao/jdbc/consumption/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO solarnode.sn_consum_datum (created, source_id, price_loc_id, watts, watt_hour)
-VALUES (?,?,?,?,?)
