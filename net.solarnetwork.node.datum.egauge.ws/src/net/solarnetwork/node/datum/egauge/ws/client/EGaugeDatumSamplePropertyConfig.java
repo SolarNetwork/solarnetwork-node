1d7617d1a3034cfd880153754565cc44d80bb791
Matt Magoffin <matt@solarnetwork.net>
2018-03-23 07:34:54 +1300

    Ensure the default property values match the published default settings, to avoid a NPE from a missing propertyType value.

--- net.solarnetwork.node.datum.egauge.ws/src/net/solarnetwork/node/datum/egauge/ws/client/EGaugeDatumSamplePropertyConfig.java
+++ net.solarnetwork.node.datum.egauge.ws/src/net/solarnetwork/node/datum/egauge/ws/client/EGaugeDatumSamplePropertyConfig.java
@@ -36,6 +36,7 @@ public class EGaugeDatumSamplePropertyConfig
 
 	public EGaugeDatumSamplePropertyConfig() {
 		super();
+		setPropertyType(GeneralDatumSamplesType.Instantaneous);
 		setConfig(new EGaugePropertyConfig());
 	}
 
