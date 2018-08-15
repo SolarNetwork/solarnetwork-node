01fe29547035afce13ebb6ed838f9a24b4115ee3
Matt Magoffin <matt@solarnetwork.net>
2018-08-16 07:11:37 +1200

    Return the first available data from the DatumDataSource API; let the client validate the input arguments.

--- net.solarnetwork.node.weather.ibm.wc/src/net/solarnetwork/node/weather/ibm/wc/WCDayDatumDataSource.java
+++ net.solarnetwork.node.weather.ibm.wc/src/net/solarnetwork/node/weather/ibm/wc/WCDayDatumDataSource.java
@@ -25,8 +25,6 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import net.solarnetwork.node.DatumDataSource;
 import net.solarnetwork.node.MultiDatumDataSource;
 import net.solarnetwork.node.domain.GeneralDayDatum;
@@ -46,10 +44,10 @@ public class WCDayDatumDataSource extends WCSupport<GeneralDayDatum>
 		implements DatumDataSource<GeneralDayDatum>, MultiDatumDataSource<GeneralDayDatum>,
 		SettingSpecifierProvider {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
 	private static final String[] DEFAULT_MENU = new String[] { "3day", "5day", "7day", "10day",
 			"15day" };
 
+	@Override
 	public List<SettingSpecifier> getSettingSpecifiers() {
 		List<SettingSpecifier> result = new ArrayList<SettingSpecifier>(1);
 		result.add(new BasicTextFieldSettingSpecifier("uid", null));
@@ -66,10 +64,12 @@ public class WCDayDatumDataSource extends WCSupport<GeneralDayDatum>
 		return result;
 	}
 
+	@Override
 	public String getSettingUID() {
 		return "net.solarnetwork.node.weather.ibm.wc.day";
 	}
 
+	@Override
 	public String getDisplayName() {
 		return "IBM daily weather information";
 	}
@@ -81,8 +81,8 @@ public class WCDayDatumDataSource extends WCSupport<GeneralDayDatum>
 
 	@Override
 	public GeneralDayDatum readCurrentDatum() {
-
-		return null;
+		Collection<GeneralDayDatum> results = readMultipleDatum();
+		return (results.isEmpty() ? null : results.iterator().next());
 	}
 
 	@Override
@@ -92,15 +92,8 @@ public class WCDayDatumDataSource extends WCSupport<GeneralDayDatum>
 
 	@Override
 	public Collection<GeneralDayDatum> readMultipleDatum() {
-
-		if ( this.getLocationIdentifier() != null && this.getApiKey() != null
-				&& this.getDatumPeriod() != null ) {
-			return this.getClient().readDailyForecast(this.getLocationIdentifier(), this.getApiKey(),
-					DailyDatumPeriod.forPeriod(this.getDatumPeriod()));
-		}
-		log.error("Unable to retrieve datum because of incorrect configuration");
-
-		return null;
+		return this.getClient().readDailyForecast(this.getLocationIdentifier(), this.getApiKey(),
+				DailyDatumPeriod.forPeriod(this.getDatumPeriod()));
 	}
 
 }
--- net.solarnetwork.node.weather.ibm.wc/src/net/solarnetwork/node/weather/ibm/wc/WCHourlyDatumDataSource.java
+++ net.solarnetwork.node.weather.ibm.wc/src/net/solarnetwork/node/weather/ibm/wc/WCHourlyDatumDataSource.java
@@ -25,8 +25,6 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import net.solarnetwork.node.DatumDataSource;
 import net.solarnetwork.node.MultiDatumDataSource;
 import net.solarnetwork.node.settings.SettingSpecifier;
@@ -47,8 +45,7 @@ public class WCHourlyDatumDataSource extends WCSupport<WCHourlyDatum> implements
 	private static final String[] DEFAULT_MENU = new String[] { "6hour", "12hour", "1day", "2day",
 			"3day", "10day", "15day" };
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-
+	@Override
 	public List<SettingSpecifier> getSettingSpecifiers() {
 		List<SettingSpecifier> result = new ArrayList<SettingSpecifier>(1);
 		result.add(new BasicTextFieldSettingSpecifier("uid", null));
@@ -65,10 +62,12 @@ public class WCHourlyDatumDataSource extends WCSupport<WCHourlyDatum> implements
 		return result;
 	}
 
+	@Override
 	public String getSettingUID() {
 		return "net.solarnetwork.node.weather.ibm.wc.hour";
 	}
 
+	@Override
 	public String getDisplayName() {
 		return "IBM Weather Channel Hourly Weather";
 	}
@@ -80,13 +79,8 @@ public class WCHourlyDatumDataSource extends WCSupport<WCHourlyDatum> implements
 
 	@Override
 	public Collection<WCHourlyDatum> readMultipleDatum() {
-		if ( this.getLocationIdentifier() != null && this.getApiKey() != null
-				&& this.getDatumPeriod() != null ) {
-			return this.getClient().readHourlyForecast(this.getLocationIdentifier(), this.getApiKey(),
-					HourlyDatumPeriod.forPeriod(this.getDatumPeriod()));
-		}
-		log.error("Unable to retrieve datum because of incorrect configuration");
-		return null;
+		return this.getClient().readHourlyForecast(this.getLocationIdentifier(), this.getApiKey(),
+				HourlyDatumPeriod.forPeriod(this.getDatumPeriod()));
 	}
 
 	@Override
@@ -96,6 +90,7 @@ public class WCHourlyDatumDataSource extends WCSupport<WCHourlyDatum> implements
 
 	@Override
 	public WCHourlyDatum readCurrentDatum() {
-		return null;
+		Collection<WCHourlyDatum> results = readMultipleDatum();
+		return (results.isEmpty() ? null : results.iterator().next());
 	}
 }
