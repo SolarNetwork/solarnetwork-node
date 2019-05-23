2fb85e0abf91dcc5bcf11e3ab6dc410341a16af5
Matt Magoffin <matt@solarnetwork.net>
2019-05-23 17:38:31 +1200

    Redirect STDIN from /dev/null to keep accidental sudo asking for password.

--- net.solarnetwork.node.setup.deb/src/net/solarnetwork/node/setup/deb/DebianPlatformPackageService.java
+++ net.solarnetwork.node.setup.deb/src/net/solarnetwork/node/setup/deb/DebianPlatformPackageService.java
@@ -27,7 +27,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -78,6 +80,10 @@ public class DebianPlatformPackageService extends BasePlatformPackageService
 				log.info("Extracting Debian archive {}", archive);
 				List<Path> extractedPaths = new ArrayList<>();
 				ProcessBuilder pb = new ProcessBuilder(cmd);
+				Path devNull = Paths.get("/dev/null");
+				if ( Files.exists(devNull) ) {
+					pb.redirectInput(devNull.toFile());
+				}
 				Process pr = pb.start();
 				try (BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 						BufferedReader err = new BufferedReader(
