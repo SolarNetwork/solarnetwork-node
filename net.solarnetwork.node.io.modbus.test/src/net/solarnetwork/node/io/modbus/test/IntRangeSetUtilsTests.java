61732dfc42e637a92bda8cf1fff490c2f882cc7c
Matt Magoffin <matt@solarnetwork.net>
2019-08-29 15:19:48 +1200

    Add utility to generate modbus register address sets in a generic way.

--- net.solarnetwork.node.io.modbus.test/src/net/solarnetwork/node/io/modbus/test/IntRangeSetUtilsTests.java
+++ net.solarnetwork.node.io.modbus.test/src/net/solarnetwork/node/io/modbus/test/IntRangeSetUtilsTests.java
@@ -22,6 +22,8 @@
 
 package net.solarnetwork.node.io.modbus.test;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.equalTo;
@@ -29,16 +31,20 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import java.util.HashSet;
 import org.junit.Test;
 import bak.pcj.set.IntRange;
 import bak.pcj.set.IntRangeSet;
 import net.solarnetwork.node.io.modbus.IntRangeSetUtils;
+import net.solarnetwork.node.io.modbus.ModbusDataType;
+import net.solarnetwork.node.io.modbus.ModbusReadFunction;
+import net.solarnetwork.node.io.modbus.ModbusReference;
 
 /**
  * Test cases for the {@link IntRangeSetUtils} class.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class IntRangeSetUtilsTests {
 
@@ -87,4 +93,90 @@ public class IntRangeSetUtilsTests {
 		assertThat("Range 1", ranges[1], equalTo(new IntRange(404, 418)));
 	}
 
+	private static enum FooRegister implements ModbusReference {
+		InfoOne(0, ModbusDataType.UInt16),
+
+		InfoTwo(1, 5, ModbusDataType.StringUtf8),
+
+		SetupThree(6, ModbusDataType.UInt16),
+
+		ThingFour(7, ModbusDataType.UInt16);
+
+		private final int address;
+		private final int length;
+		private final ModbusDataType dataType;
+
+		private FooRegister(int address, ModbusDataType dataType) {
+			this(address, 0, dataType);
+		}
+
+		private FooRegister(int address, int length, ModbusDataType dataType) {
+			this.address = address;
+			this.length = length;
+			this.dataType = dataType;
+		}
+
+		@Override
+		public int getAddress() {
+			return address;
+		}
+
+		@Override
+		public ModbusDataType getDataType() {
+			return dataType;
+		}
+
+		@Override
+		public ModbusReadFunction getFunction() {
+			return ModbusReadFunction.ReadInputRegister;
+		}
+
+		@Override
+		public int getWordLength() {
+			return (this.length > 0 ? this.length : dataType.getWordLength());
+		}
+
+	}
+
+	@Test
+	public void enumAddressSet_All() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class, null);
+		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 7) }));
+	}
+
+	@Test
+	public void enumAddressSet_Info() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
+				singleton("Info"));
+		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 5) }));
+	}
+
+	@Test
+	public void enumAddressSet_InfoAndSetup() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
+				new HashSet<>(asList("Info", "Setup")));
+		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 6) }));
+	}
+
+	@Test
+	public void enumAddressSet_InfoAndSetupAndThing() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
+				new HashSet<>(asList("Info", "Setup", "Thing")));
+		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 7) }));
+	}
+
+	@Test
+	public void enumAddressSet_InfoAndThing() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
+				new HashSet<>(asList("Info", "Thing")));
+		assertThat("Resolved ranges", result.ranges(),
+				equalTo(new IntRange[] { new IntRange(0, 5), new IntRange(7, 7) }));
+	}
+
+	@Test
+	public void enumAddressSet_NoMatch() {
+		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
+				new HashSet<>(asList("Hello")));
+		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[0]));
+	}
 }
