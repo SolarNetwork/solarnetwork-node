40a45cab88bf5cfe3d73bd842959cfc3e94aa1d3
Matt Magoffin <matt@solarnetwork.net>
2019-09-27 16:13:07 +1200

    Work on CAN bus property settings.

--- net.solarnetwork.node.datum.canbus/src/net/solarnetwork/node/datum/canbus/CanbusPropertyConfig.java
+++ net.solarnetwork.node.datum.canbus/src/net/solarnetwork/node/datum/canbus/CanbusPropertyConfig.java
@@ -22,13 +22,12 @@
 
 package net.solarnetwork.node.datum.canbus;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import net.solarnetwork.domain.GeneralDatumSamplePropertyConfig;
 import net.solarnetwork.domain.GeneralDatumSamplesType;
+import net.solarnetwork.domain.NumberDatumSamplePropertyConfig;
 import net.solarnetwork.node.settings.SettingSpecifier;
 import net.solarnetwork.node.settings.support.BasicMultiValueSettingSpecifier;
 import net.solarnetwork.node.settings.support.BasicTextFieldSettingSpecifier;
@@ -43,16 +42,18 @@ import net.solarnetwork.node.settings.support.BasicTextFieldSettingSpecifier;
  * @author matt
  * @version 1.0
  */
-public class CanbusPropertyConfig extends GeneralDatumSamplePropertyConfig<Integer> {
+public class CanbusPropertyConfig extends NumberDatumSamplePropertyConfig<Integer> {
 
-	private BigDecimal unitMultiplier;
-	private int decimalScale;
+	/** The default {@code address} property value. */
+	public static final int DEFAULT_ADDRESS = 0;
+
+	private String name;
 
 	/**
 	 * Default constructor.
 	 */
 	public CanbusPropertyConfig() {
-		super(null, GeneralDatumSamplesType.Instantaneous, 0);
+		super(null, GeneralDatumSamplesType.Instantaneous, DEFAULT_ADDRESS);
 	}
 
 	/**
@@ -70,45 +71,6 @@ public class CanbusPropertyConfig extends GeneralDatumSamplePropertyConfig<Integ
 	}
 
 	/**
-	 * Construct with values.
-	 * 
-	 * @param name
-	 *        the datum property name
-	 * @param dataType
-	 *        the CAN bus data type
-	 * @param address
-	 *        the CAN bus register address
-	 * @param unitMultiplier
-	 *        the unit multiplier
-	 */
-	public CanbusPropertyConfig(String name, GeneralDatumSamplesType datumPropertyType, int address,
-			BigDecimal unitMultiplier) {
-		this(name, datumPropertyType, address, unitMultiplier, 0);
-	}
-
-	/**
-	 * Construct with values.
-	 * 
-	 * @param name
-	 *        the datum property name
-	 * @param datumPropertyType
-	 *        the datum property type
-	 * @param address
-	 *        the CAN bus register address
-	 * @param unitMultiplier
-	 *        the unit multiplier
-	 * @param decimalScale
-	 *        for numbers, the maximum decimal scale to support, or
-	 *        {@literal -1} for no limit
-	 */
-	public CanbusPropertyConfig(String name, GeneralDatumSamplesType datumPropertyType, int address,
-			BigDecimal unitMultiplier, int decimalScale) {
-		super(name, datumPropertyType, address);
-		this.unitMultiplier = unitMultiplier;
-		this.decimalScale = decimalScale;
-	}
-
-	/**
 	 * Get settings suitable for configuring an instance of this class.
 	 * 
 	 * @param prefix
@@ -116,14 +78,14 @@ public class CanbusPropertyConfig extends GeneralDatumSamplePropertyConfig<Integ
 	 * @return the settings, never {@literal null}
 	 */
 	public static List<SettingSpecifier> settings(String prefix) {
-		CanbusPropertyConfig defaults = new CanbusPropertyConfig();
 		List<SettingSpecifier> results = new ArrayList<SettingSpecifier>(8);
 
 		results.add(new BasicTextFieldSettingSpecifier(prefix + "name", ""));
+		results.add(new BasicTextFieldSettingSpecifier(prefix + "propertyKey", ""));
 
-		// drop-down menu for datumPropertyType
+		// drop-down menu for propertyTypeKey
 		BasicMultiValueSettingSpecifier propTypeSpec = new BasicMultiValueSettingSpecifier(
-				prefix + "datumPropertyTypeKey", defaults.getDatumPropertyTypeKey());
+				prefix + "propertyTypeKey", String.valueOf(DEFAULT_PROPERTY_TYPE.toKey()));
 		Map<String, String> propTypeTitles = new LinkedHashMap<String, String>(3);
 		for ( GeneralDatumSamplesType e : GeneralDatumSamplesType.values() ) {
 			propTypeTitles.put(Character.toString(e.toKey()), e.toString());
@@ -131,120 +93,18 @@ public class CanbusPropertyConfig extends GeneralDatumSamplePropertyConfig<Integ
 		propTypeSpec.setValueTitles(propTypeTitles);
 		results.add(propTypeSpec);
 
-		results.add(new BasicTextFieldSettingSpecifier(prefix + "address",
-				String.valueOf(defaults.getAddress())));
+		results.add(
+				new BasicTextFieldSettingSpecifier(prefix + "address", String.valueOf(DEFAULT_ADDRESS)));
 
-		results.add(new BasicTextFieldSettingSpecifier(prefix + "unitMultiplier", "1"));
-		results.add(new BasicTextFieldSettingSpecifier(prefix + "decimalScale", "0"));
+		results.add(new BasicTextFieldSettingSpecifier(prefix + "unitIntercept",
+				DEFAULT_INTERCEPT.toString()));
+		results.add(new BasicTextFieldSettingSpecifier(prefix + "unitSlope", DEFAULT_SLOPE.toString()));
+		results.add(new BasicTextFieldSettingSpecifier(prefix + "slope", DEFAULT_SLOPE.toString()));
+		results.add(
+				new BasicTextFieldSettingSpecifier(prefix + "intercept", DEFAULT_INTERCEPT.toString()));
+		results.add(new BasicTextFieldSettingSpecifier(prefix + "decimalScale",
+				String.valueOf(DEFAULT_DECIMAL_SCALE)));
 		return results;
-	}
-
-	/**
-	 * Get the datum property name used for this configuration.
-	 * 
-	 * <p>
-	 * This is an alias for {@link #getPropertyKey()}.
-	 * </p>
-	 * 
-	 * @return the property name
-	 */
-	public String getName() {
-		return getPropertyKey();
-	}
-
-	/**
-	 * Set the datum property name to use.
-	 * 
-	 * <p>
-	 * This is an alias for {@link #setPropertyKey(String)}.
-	 * </p>
-	 * 
-	 * @param name
-	 *        the property name
-	 */
-	public void setName(String name) {
-		setPropertyKey(name);
-	}
-
-	/**
-	 * Get the datum property type.
-	 * 
-	 * <p>
-	 * This is an alias for {@link #getPropertyType()}.
-	 * </p>
-	 * 
-	 * @return the type
-	 */
-	public GeneralDatumSamplesType getDatumPropertyType() {
-		return getPropertyType();
-	}
-
-	/**
-	 * Set the datum property type.
-	 * 
-	 * <p>
-	 * This is an alias for {@link #setPropertyType(GeneralDatumSamplesType)},
-	 * and ignores a {@literal null} argument.
-	 * </p>
-	 * </p>
-	 * 
-	 * @param datumPropertyType
-	 *        the datum property type to set
-	 */
-	public void setDatumPropertyType(GeneralDatumSamplesType datumPropertyType) {
-		if ( datumPropertyType == null ) {
-			return;
-		}
-		setPropertyType(datumPropertyType);
-	}
-
-	/**
-	 * Get the property type key.
-	 * 
-	 * <p>
-	 * This returns the configured {@link #getPropertyType()}
-	 * {@link GeneralDatumSamplesType#toKey()} value as a string. If the type is
-	 * not available, {@link GeneralDatumSamplesType#Instantaneous} will be
-	 * returned.
-	 * </p>
-	 * 
-	 * @return the property type key
-	 */
-	public String getDatumPropertyTypeKey() {
-		GeneralDatumSamplesType type = getDatumPropertyType();
-		if ( type == null ) {
-			type = GeneralDatumSamplesType.Instantaneous;
-		}
-		return Character.toString(type.toKey());
-	}
-
-	/**
-	 * Set the property type via a key value.
-	 * 
-	 * <p>
-	 * This uses the first character of {@code key} as a
-	 * {@link GeneralDatumSamplesType} key value to call
-	 * {@link #setPropertyType(GeneralDatumSamplesType)}. If there is any
-	 * problem parsing the type, {@link GeneralDatumSamplesType#Instantaneous}
-	 * is set.
-	 * </p>
-	 * 
-	 * @param key
-	 *        the datum property type key to set
-	 */
-	public void setDatumPropertyTypeKey(String key) {
-		GeneralDatumSamplesType type = null;
-		if ( key != null && key.length() > 0 ) {
-			try {
-				type = GeneralDatumSamplesType.valueOf(key.charAt(0));
-			} catch ( IllegalArgumentException e ) {
-				// ignore
-			}
-		}
-		if ( type == null ) {
-			type = GeneralDatumSamplesType.Instantaneous;
-		}
-		setDatumPropertyType(type);
 	}
 
 	/**
@@ -277,56 +137,22 @@ public class CanbusPropertyConfig extends GeneralDatumSamplePropertyConfig<Integ
 	}
 
 	/**
-	 * Get the unit multiplier.
+	 * Get a friendly name for the address.
 	 * 
-	 * @return the multiplier
+	 * @return the name
 	 */
-	public BigDecimal getUnitMultiplier() {
-		return unitMultiplier;
+	public String getName() {
+		return name;
 	}
 
 	/**
-	 * Set the unit multiplier.
+	 * Set the friendly name for the address.
 	 * 
-	 * <p>
-	 * This value represents a multiplication factor to apply to values
-	 * collected for this property so that a standardized unit is captured. For
-	 * example, a power meter might report power as <i>killowatts</i>, in which
-	 * case {@code multiplier} can be configured as {@literal .001} to convert
-	 * the value to <i>watts</i>.
-	 * </p>
-	 * 
-	 * @param unitMultiplier
-	 *        the mutliplier to set
+	 * @param name
+	 *        the name to set
 	 */
-	public void setUnitMultiplier(BigDecimal unitMultiplier) {
-		this.unitMultiplier = unitMultiplier;
-	}
-
-	/**
-	 * Get the decimal scale to round decimal numbers to.
-	 * 
-	 * @return the decimal scale
-	 */
-	public int getDecimalScale() {
-		return decimalScale;
-	}
-
-	/**
-	 * Set the decimal scale to round decimal numbers to.
-	 * 
-	 * <p>
-	 * This is a <i>maximum</i> scale value that decimal values should be
-	 * rounded to. This is applied <i>after</i> any {@code unitMultiplier} is
-	 * applied. A scale of {@literal 0} would round all decimals to integer
-	 * values.
-	 * </p>
-	 * 
-	 * @param decimalScale
-	 *        the scale to set, or {@literal -1} to disable rounding completely
-	 */
-	public void setDecimalScale(int decimalScale) {
-		this.decimalScale = decimalScale;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
