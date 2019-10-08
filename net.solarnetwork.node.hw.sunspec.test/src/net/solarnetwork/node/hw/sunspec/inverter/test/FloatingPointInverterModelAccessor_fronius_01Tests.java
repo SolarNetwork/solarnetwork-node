652b4e22836064bfc69419bf118e8261f12c6ba0
Matt Magoffin <matt@solarnetwork.net>
2019-10-08 16:10:56 +1300

    Add TODO test case for a floating-point inverter data set.

---
+++ net.solarnetwork.node.hw.sunspec.test/src/net/solarnetwork/node/hw/sunspec/inverter/test/FloatingPointInverterModelAccessor_fronius_01Tests.java
@@ -0,0 +1,79 @@
+/* ==================================================================
+ * FloatingPointInverterModelAccessor_fronius_01Tests.java - 8/10/2019 3:36:49 pm
+ * 
+ * Copyright 2019 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.hw.sunspec.inverter.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.solarnetwork.node.hw.sunspec.ModelData;
+import net.solarnetwork.node.hw.sunspec.inverter.IntegerInverterModelAccessor;
+import net.solarnetwork.node.hw.sunspec.meter.test.IntegerMeterModelAccessorTests;
+import net.solarnetwork.node.hw.sunspec.test.ModelDataUtils;
+
+/**
+ * Test cases for the {@link IntegerInverterModelAccessor} class.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class FloatingPointInverterModelAccessor_fronius_01Tests {
+
+	private static final Logger log = LoggerFactory.getLogger(IntegerMeterModelAccessorTests.class);
+
+	private ModelData getTestDataInstance() {
+		return ModelDataUtils.getModelDataInstance(getClass(), "fronius-01.txt");
+	}
+
+	@Test
+	public void dataDebugString() {
+		ModelData data = getTestDataInstance();
+		log.debug("Got test data: " + data.dataDebugString());
+	}
+
+	/*- TODO
+	@Test
+	public void commonModelProperties() {
+		CommonModelAccessor data = getTestDataInstance();
+		assertThat("Manufacturer", data.getManufacturer(), equalTo("SolarEdge"));
+		assertThat("Model name", data.getModelName(), equalTo("SE33.3K"));
+		assertThat("Options", data.getOptions(), equalTo(""));
+		assertThat("Version", data.getVersion(), equalTo("0003.2251"));
+		assertThat("Serial number", data.getSerialNumber(), equalTo("7E1240CC"));
+		assertThat("Device address", data.getDeviceAddress(), equalTo(11));
+	}
+	
+	@Test
+	public void findTypedModel() {
+		ModelData data = getTestDataInstance();
+		InverterModelAccessor meterAccessor = data.findTypedModel(InverterModelAccessor.class);
+		assertThat(meterAccessor, instanceOf(IntegerInverterModelAccessor.class));
+	}
+	
+	@Test
+	public void getTypedModel() {
+		ModelData data = getTestDataInstance();
+		InverterModelAccessor meterAccessor = data.getTypedModel();
+		assertThat(meterAccessor, instanceOf(IntegerInverterModelAccessor.class));
+	}
+	*/
+}
