5fe6146931f9c99b3234ffcefdfd097154dd8fe5
Matt Magoffin <matt@solarnetwork.net>
2020-01-17 11:48:47 +1300

    NODE-180: remove PCJ from IntRangeSetUtils.

--- net.solarnetwork.node.io.modbus.test/src/net/solarnetwork/node/io/modbus/test/IntRangeSetUtilsTests.java
+++ net.solarnetwork.node.io.modbus.test/src/net/solarnetwork/node/io/modbus/test/IntRangeSetUtilsTests.java
@@ -24,76 +24,30 @@ package net.solarnetwork.node.io.modbus.test;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.arrayWithSize;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.sameInstance;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
+import static net.solarnetwork.util.IntRange.rangeOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import java.util.HashSet;
 import org.junit.Test;
-import bak.pcj.set.IntRange;
-import bak.pcj.set.IntRangeSet;
 import net.solarnetwork.node.io.modbus.IntRangeSetUtils;
 import net.solarnetwork.node.io.modbus.ModbusDataType;
 import net.solarnetwork.node.io.modbus.ModbusReadFunction;
 import net.solarnetwork.node.io.modbus.ModbusReference;
+import net.solarnetwork.util.IntRangeSet;
 
 /**
  * Test cases for the {@link IntRangeSetUtils} class.
  * 
  * @author matt
- * @version 1.1
+ * @version 2.0
  */
 public class IntRangeSetUtilsTests {
 
-	@Test
-	public void combineToReduceSizeReduced() {
-		IntRangeSet set = new IntRangeSet();
-		set.addAll(0, 1);
-		set.addAll(3, 5);
-		set.addAll(100, 101);
-		assertThat("Original range count", set.ranges(), arrayWithSize(3));
-		IntRangeSet result = IntRangeSetUtils.combineToReduceSize(set, 64);
-		assertThat("New copy returned", result, allOf(notNullValue(), not(sameInstance(set))));
-		assertThat("New range count", result.ranges(), arrayWithSize(2));
-		IntRange[] ranges = result.ranges();
-		assertThat("Range 0", ranges[0], equalTo(new IntRange(0, 5)));
-		assertThat("Range 1", ranges[1], equalTo(new IntRange(100, 101)));
-	}
-
-	@Test
-	public void combineToReduceSizeNotReduced() {
-		IntRangeSet set = new IntRangeSet();
-		set.addAll(0, 5);
-		set.addAll(100, 101);
-		assertThat("Original range count", set.ranges(), arrayWithSize(2));
-		IntRangeSet result = IntRangeSetUtils.combineToReduceSize(set, 64);
-		assertThat("New copy returned", result, allOf(notNullValue(), not(sameInstance(set))));
-		assertThat("New range count", result.ranges(), arrayWithSize(2));
-		IntRange[] ranges = result.ranges();
-		assertThat("Range 0", ranges[0], equalTo(new IntRange(0, 5)));
-		assertThat("Range 1", ranges[1], equalTo(new IntRange(100, 101)));
-	}
-
-	@Test
-	public void javaDocExample() {
-		IntRangeSet set = new IntRangeSet();
-		set.addAll(0, 1);
-		set.addAll(3, 5);
-		set.addAll(20, 28);
-		set.addAll(404, 406);
-		set.addAll(412, 418);
-		IntRangeSet result = IntRangeSetUtils.combineToReduceSize(set, 64);
-		assertThat("New copy returned", result, allOf(notNullValue(), not(sameInstance(set))));
-		assertThat("New range count", result.ranges(), arrayWithSize(2));
-		IntRange[] ranges = result.ranges();
-		assertThat("Range 0", ranges[0], equalTo(new IntRange(0, 28)));
-		assertThat("Range 1", ranges[1], equalTo(new IntRange(404, 418)));
-	}
-
 	private static enum FooRegister implements ModbusReference {
+
 		InfoOne(0, ModbusDataType.UInt16),
 
 		InfoTwo(1, 5, ModbusDataType.StringUtf8),
@@ -141,42 +95,42 @@ public class IntRangeSetUtilsTests {
 	@Test
 	public void enumAddressSet_All() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class, null);
-		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 7) }));
+		assertThat("Resolved ranges", result.ranges(), contains(rangeOf(0, 7)));
 	}
 
 	@Test
 	public void enumAddressSet_Info() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
 				singleton("Info"));
-		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 5) }));
+		assertThat("Resolved ranges", result.ranges(), contains(rangeOf(0, 5)));
 	}
 
 	@Test
 	public void enumAddressSet_InfoAndSetup() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
 				new HashSet<>(asList("Info", "Setup")));
-		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 6) }));
+		assertThat("Resolved ranges", result.ranges(), contains(rangeOf(0, 6)));
 	}
 
 	@Test
 	public void enumAddressSet_InfoAndSetupAndThing() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
 				new HashSet<>(asList("Info", "Setup", "Thing")));
-		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[] { new IntRange(0, 7) }));
+		assertThat("Resolved ranges", result.ranges(), contains(rangeOf(0, 7)));
 	}
 
 	@Test
 	public void enumAddressSet_InfoAndThing() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
 				new HashSet<>(asList("Info", "Thing")));
-		assertThat("Resolved ranges", result.ranges(),
-				equalTo(new IntRange[] { new IntRange(0, 5), new IntRange(7, 7) }));
+		assertThat("Resolved ranges", result.ranges(), contains(rangeOf(0, 5), rangeOf(7, 7)));
 	}
 
 	@Test
 	public void enumAddressSet_NoMatch() {
 		IntRangeSet result = IntRangeSetUtils.createRegisterAddressSet(FooRegister.class,
 				new HashSet<>(asList("Hello")));
-		assertThat("Resolved ranges", result.ranges(), equalTo(new IntRange[0]));
+		assertThat("Resolved ranges", stream(result.ranges().spliterator(), false).collect(toList()),
+				hasSize(0));
 	}
 }
