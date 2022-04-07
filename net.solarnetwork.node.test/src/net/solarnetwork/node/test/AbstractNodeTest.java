1fe7409a63fc1910c96e56a473bbfbd56db35b64
Matt Magoffin <matt@solarnetwork.net>
2022-04-07 16:42:57 +1200

    Add support for creating EmbeddedDatabase instances.

--- net.solarnetwork.node.test/src/net/solarnetwork/node/test/AbstractNodeTest.java
+++ net.solarnetwork.node.test/src/net/solarnetwork/node/test/AbstractNodeTest.java
@@ -22,8 +22,14 @@
 
 package net.solarnetwork.node.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
@@ -31,12 +37,44 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
  * Base test class for non-transactional unit tests.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 @ContextConfiguration
 public abstract class AbstractNodeTest extends AbstractJUnit4SpringContextTests {
 
 	/** A class-level logger. */
 	protected final Logger log = LoggerFactory.getLogger(getClass());
+
+	protected final Properties envProperties = new Properties();
+
+	@Before
+	public void loadEnvironmentProperties() {
+		try (InputStream in = getClass().getClassLoader().getResourceAsStream("env.properties")) {
+			envProperties.load(in);
+		} catch ( IOException e ) {
+			// we'll ignore this
+		}
+	}
+
+	/**
+	 * Create a new embedded database builder.
+	 * 
+	 * @param environmentTypeKey
+	 *        the environment property key for the database type; if not found
+	 *        {@literal DERBY} will be assumed
+	 * @return the database builder
+	 * @since 1.1
+	 */
+	protected EmbeddedDatabaseBuilder createEmbeddedDatabase(String environmentTypeKey) {
+		EmbeddedDatabaseBuilder db = new EmbeddedDatabaseBuilder().generateUniqueName(true);
+		EmbeddedDatabaseType dbType = EmbeddedDatabaseType.DERBY;
+		if ( envProperties.containsKey(environmentTypeKey) ) {
+			String prefix = envProperties.getProperty(environmentTypeKey);
+			if ( "h2".equals(prefix) ) {
+				dbType = EmbeddedDatabaseType.H2;
+			}
+		}
+		return db.setType(dbType);
+	}
 
 }
