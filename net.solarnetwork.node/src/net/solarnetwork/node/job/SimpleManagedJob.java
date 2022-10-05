0a9cab31f48792e1092ea20d9b3df5f24171d97a
Matt Magoffin <matt@solarnetwork.net>
2022-10-06 07:59:55 +1300

    Fix setting of legacy job properties directly onto JobService.

--- net.solarnetwork.node/src/net/solarnetwork/node/job/SimpleManagedJob.java
+++ net.solarnetwork.node/src/net/solarnetwork/node/job/SimpleManagedJob.java
@@ -55,7 +55,7 @@ import net.solarnetwork.support.PrefixedMessageSource;
  * Simple implementation of {@link ManagedJob}.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class SimpleManagedJob extends BaseIdentifiable
 		implements ManagedJob, SettingsChangeObserver, ServiceLifecycleObserver {
@@ -356,6 +356,21 @@ public class SimpleManagedJob extends BaseIdentifiable
 					return jobServiceAccessor.getPropertyValue(key.toString());
 				}
 				return null;
+			}
+
+			@Override
+			public Object put(String key, Object value) {
+				if ( key == null ) {
+					return null;
+				}
+				Object result = null;
+				if ( jobServiceAccessor.isWritableProperty(key) ) {
+					if ( jobServiceAccessor.isReadableProperty(key) ) {
+						result = jobServiceAccessor.getPropertyValue(key);
+					}
+					jobServiceAccessor.setPropertyValue(key, value);
+				}
+				return result;
 			}
 
 			@Override
