19bd7a1e696e1d0de626583013a4bf82d4e6dcce
Matt Magoffin <matt@solarnetwork.net>
2022-10-28 10:31:07 +1300

    Fix bug where coil registers failed to read using TCP.

--- net.solarnetwork.node.io.modbus.jamod/src/net/solarnetwork/node/io/modbus/jamod/ModbusTransactionUtils.java
+++ net.solarnetwork.node.io.modbus.jamod/src/net/solarnetwork/node/io/modbus/jamod/ModbusTransactionUtils.java
@@ -57,7 +57,7 @@ import net.wimpi.modbus.util.BitVector;
  * Utility methods for Modbus actions.
  * 
  * @author matt
- * @version 2.2
+ * @version 2.3
  */
 public class ModbusTransactionUtils {
 
@@ -89,7 +89,9 @@ public class ModbusTransactionUtils {
 		BitSet result = new BitSet(count);
 		ReadCoilsRequest req = new ReadCoilsRequest(address, count);
 		req.setUnitID(unitId);
-		req.setHeadless();
+		if ( headless ) {
+			req.setHeadless();
+		}
 		trans.setRequest(req);
 		try {
 			trans.execute();
@@ -221,7 +223,9 @@ public class ModbusTransactionUtils {
 		BitSet result = new BitSet(count);
 		ReadInputDiscretesRequest req = new ReadInputDiscretesRequest(address, count);
 		req.setUnitID(unitId);
-		req.setHeadless();
+		if ( headless ) {
+			req.setHeadless();
+		}
 		trans.setRequest(req);
 		try {
 			trans.execute();
@@ -677,8 +681,8 @@ public class ModbusTransactionUtils {
 	 * @param count
 	 *        the number of Modbus 16-bit registers to read
 	 * @param trim
-	 *        if {@literal true} then remove leading/trailing whitespace from the
-	 *        resulting string
+	 *        if {@literal true} then remove leading/trailing whitespace from
+	 *        the resulting string
 	 * @param charset
 	 *        the character set to interpret the bytes as
 	 * @return String from interpreting raw bytes as a string
