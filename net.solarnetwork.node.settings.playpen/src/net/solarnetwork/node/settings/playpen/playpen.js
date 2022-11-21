bf77bf0baa7ead6a109d1ce7bbeebe24fdc8393a
Matt Magoffin <matt@solarnetwork.net>
2022-11-22 12:12:18 +1300

    NODE-223: update settings playpen to work with new sn.settings.component.loaded event so custom JavaScript works as expected.

--- net.solarnetwork.node.settings.playpen/src/net/solarnetwork/node/settings/playpen/playpen.js
+++ net.solarnetwork.node.settings.playpen/src/net/solarnetwork/node/settings/playpen/playpen.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 	'use strict';
-	
-	$('button.playpen-setting-custom-button').on('click', function(event) {
-		var btn = $(event.target),
-			foo = btn.parent().data('foo');
-		alert("Why, hello there! foo is " +foo +"!");
-	});
 
+	function setupPlaypen(container) {
+		container.find('button.playpen-setting-custom-button').on('click', function(event) {
+			var btn = $(event.target),
+				foo = btn.parent().data('foo');
+			alert("Why, hello there! foo is " +foo +"!");
+		});
+	}
+	
+	$('body').on('sn.settings.component.loaded', function(event, container) {
+		setupPlaypen(container);
+	});	
+
+	setupPlaypen($());
 });
