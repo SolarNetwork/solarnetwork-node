95b94499f91a567f2eb9e8a0271b52340f9c40d0
Matt Magoffin <matt@solarnetwork.net>
2023-03-16 07:51:51 +1300

    Add utility to help generate docs from Enum constants.

---
+++ net.solarnetwork.node.test/src/net/solarnetwork/node/test/EnumTableGenerator.java
@@ -0,0 +1,202 @@
+/* ==================================================================
+ * EnumTableGenerator.java - 16/03/2023 6:53:37 am
+ * 
+ * Copyright 2023 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.test;
+
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import net.solarnetwork.domain.Bitmaskable;
+import net.solarnetwork.domain.CodedValue;
+import net.solarnetwork.domain.GroupedBitmaskable;
+
+/**
+ * Utility to generate a Markdown table out of enum descriptions.
+ * 
+ * <p>
+ * Special support for {@link GroupedBitmaskable}, {@link Bitmaskable}, and
+ * {@link CodedValue} is provided. If an enum class implements one of those
+ * interfaces, the generated index will be derived from
+ * {@link GroupedBitmaskable#getOverallIndex()},
+ * {@link Bitmaskable#bitmaskBitOffset()}, or {@link CodedValue#getCode()}.
+ * </p>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class EnumTableGenerator {
+
+	private static final String[] HEADERS = new String[] { "Index", "Name", "Description" };
+
+	private final int[] widths = new int[] { HEADERS[0].length(), HEADERS[1].length(),
+			HEADERS[2].length() };
+	private final List<Class<? extends Enum<?>>> enumClasses = new ArrayList<>(4);
+	private final List<Method> descriptionMethods = new ArrayList<>(4);
+	private final PrintStream out;
+
+	private final List<String[]> rows = new ArrayList<>(16);
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param out
+	 *        the output stream
+	 */
+	public EnumTableGenerator(PrintStream out) {
+		super();
+		this.out = Objects.requireNonNull(out);
+	}
+
+	/**
+	 * Add an enum.
+	 * 
+	 * @param enumClass
+	 *        the enum class to add
+	 */
+	public void add(Class<? extends Enum<?>> enumClass) {
+		enumClasses.add(enumClass);
+		descriptionMethods.add(descriptionMethod(enumClass));
+	}
+
+	private static final Method descriptionMethod(Class<? extends Enum<?>> enumClass) {
+		try {
+			return enumClass.getMethod("getDescription");
+		} catch ( NoSuchMethodException | SecurityException e ) {
+			// ignore
+		}
+		return null;
+	}
+
+	private int index(Enum<?> e) {
+		if ( e instanceof GroupedBitmaskable ) {
+			return ((GroupedBitmaskable) e).getOverallIndex();
+		} else if ( e instanceof Bitmaskable ) {
+			return ((Bitmaskable) e).bitmaskBitOffset();
+		} else if ( e instanceof CodedValue ) {
+			return ((CodedValue) e).getCode();
+		}
+		return e.ordinal();
+	}
+
+	private String description(int i, Enum<?> e) {
+		Method m = descriptionMethods.get(i);
+		if ( m != null ) {
+			try {
+				Object desc = m.invoke(e);
+				if ( desc != null ) {
+					return desc.toString();
+				}
+			} catch ( IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e1 ) {
+				// ignore
+			}
+		}
+		return e.toString();
+	}
+
+	/**
+	 * Generate the Markdown table.
+	 */
+	public void generate() {
+		for ( int enumIndex = 0, enumCount = enumClasses.size(); enumIndex < enumCount; enumIndex++ ) {
+			Enum<?>[] enums = enumClasses.get(enumIndex).getEnumConstants();
+
+			// calculate row content
+			for ( int i = 0, len = enums.length; i < len; i++ ) {
+				final String[] row = new String[3];
+				final Enum<?> e = enums[i];
+
+				row[0] = Integer.toString(index(e));
+				int w = row[0].length();
+				if ( w > widths[0] ) {
+					widths[i] = w;
+				}
+
+				row[1] = "`" + e.name() + "`";
+				w = row[1].length();
+				if ( w > widths[1] ) {
+					widths[1] = w;
+				}
+
+				row[2] = description(enumIndex, e);
+				w = row[2].length();
+				if ( w > widths[2] ) {
+					widths[2] = w;
+				}
+
+				rows.add(row);
+			}
+		}
+
+		// print header
+		String fmt = "| %-" + widths[0] + "s | %-" + widths[1] + "s | %-" + widths[2] + "s |";
+		out.println(String.format(fmt, (Object[]) HEADERS));
+		for ( int i = 0; i < widths.length; i++ ) {
+			out.print("|:");
+			int len = widths[i] + 1;
+			for ( int j = 0; j < len; j++ ) {
+				out.print('-');
+			}
+		}
+		out.println('|');
+
+		// print rows
+		for ( String[] row : rows ) {
+			out.println(String.format(fmt, (Object[]) row));
+		}
+	}
+
+	/**
+	 * Generate a Markdown table out of enum descriptions.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if ( args.length < 1 ) {
+			System.err.println("Pass enum class name arguments to generate table(s) for.");
+			System.exit(1);
+		}
+		final EnumTableGenerator gen = new EnumTableGenerator(System.out);
+		for ( String className : args ) {
+			try {
+				@SuppressWarnings({ "rawtypes", "unchecked" })
+				Class<? extends Enum<?>> enumClass = (Class) Class.forName(className);
+				Object[] enums = enumClass.getEnumConstants();
+				if ( enums == null ) {
+					System.err.println("Class [" + className + "] is not an enum.");
+					continue;
+				}
+				gen.add(enumClass);
+			} catch ( ClassNotFoundException e ) {
+				System.err.println("Class [" + className + "] not found.");
+			} catch ( ClassCastException e ) {
+				System.err.println("Class [" + className + "] is not an enum.");
+				continue;
+			}
+		}
+		gen.generate();
+	}
+
+}
