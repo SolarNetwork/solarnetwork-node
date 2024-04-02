2a4d9436e3e4ffb04c635d17fa5bb1a90cc64498
Matt Magoffin <matt@solarnetwork.net>
2024-04-02 18:54:15 +1300

    Work on datum expression service.

---
+++ net.solarnetwork.node.internal.test/src/net/solarnetwork/node/runtime/test/SimpleDatumExpressionServiceTests.java
@@ -0,0 +1,113 @@
+/* ==================================================================
+ * SimpleDatumExpressionServiceTests.java - 2/04/2024 5:57:37 pm
+ *
+ * Copyright 2024 SolarNetwork.net Dev Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.node.runtime.test;
+
+import static java.util.Collections.singleton;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.util.AntPathMatcher;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.solarnetwork.codec.JsonUtils;
+import net.solarnetwork.common.expr.spel.SpelExpressionService;
+import net.solarnetwork.domain.InstructionStatus.InstructionState;
+import net.solarnetwork.domain.datum.DatumSamples;
+import net.solarnetwork.domain.datum.DatumSamplesType;
+import net.solarnetwork.node.domain.datum.SimpleEnergyDatum;
+import net.solarnetwork.node.reactor.Instruction;
+import net.solarnetwork.node.reactor.InstructionStatus;
+import net.solarnetwork.node.reactor.InstructionUtils;
+import net.solarnetwork.node.runtime.DefaultDatumService;
+import net.solarnetwork.node.runtime.SimpleDatumExpressionService;
+import net.solarnetwork.node.service.DatumMetadataService;
+import net.solarnetwork.service.ExpressionService;
+import net.solarnetwork.service.StaticOptionalService;
+import net.solarnetwork.service.StaticOptionalServiceCollection;
+
+/**
+ * Test cases for the {@link SimpleDatumExpressionService} class.
+ *
+ * @author matt
+ * @version 1.0
+ */
+public class SimpleDatumExpressionServiceTests {
+
+	private SpelExpressionService spel;
+	private DefaultDatumService datumService;
+	private SimpleDatumExpressionService service;
+
+	@Before
+	public void setup() {
+		AntPathMatcher pathMatcher = new AntPathMatcher();
+		pathMatcher.setCaseSensitive(false);
+		pathMatcher.setCachePatterns(false);
+
+		ObjectMapper mapper = JsonUtils.newDatumObjectMapper();
+
+		datumService = new DefaultDatumService(pathMatcher, mapper,
+				new StaticOptionalService<DatumMetadataService>(null));
+
+		spel = new SpelExpressionService();
+
+		service = new SimpleDatumExpressionService(datumService);
+		service.setExpressionServices(
+				new StaticOptionalServiceCollection<ExpressionService>(singleton(spel)));
+	}
+
+	@Test
+	public void executeExpression() {
+		// GIVEN
+		final Integer watts = 1234;
+
+		final SimpleEnergyDatum d = new SimpleEnergyDatum("/power/1", Instant.now(), new DatumSamples());
+		d.putSampleValue(DatumSamplesType.Instantaneous, "watts", watts);
+		datumService.accept(d);
+
+		final Map<String, String> instrParams = new LinkedHashMap<>(4);
+		instrParams.put(SimpleDatumExpressionService.PARAM_EXPRESSION_LANGUAGE, spel.getUid());
+		instrParams.put(SimpleDatumExpressionService.PARAM_EXPRESSION, "latest('/power/1').watts");
+		final Instruction instr = InstructionUtils.createLocalInstruction(
+				SimpleDatumExpressionService.TOPIC_DATUM_EXPRESSION, instrParams);
+
+		// WHEN
+		InstructionStatus result = service.processInstruction(instr);
+
+		// THEN
+		assertThat("Result returned", result, is(notNullValue()));
+		assertThat("Result is completed", result.getInstructionState(),
+				is(equalTo(InstructionState.Completed)));
+
+		Map<String, ?> resultParams = result.getResultParameters();
+		assertThat("Expression result provided as JSON array on 'result' result parameter", resultParams,
+				hasEntry("result", Arrays.asList(1234)));
+	}
+
+}
--- net.solarnetwork.node/src/net/solarnetwork/node/runtime/SimpleDatumExpressionService.java
+++ net.solarnetwork.node/src/net/solarnetwork/node/runtime/SimpleDatumExpressionService.java
@@ -22,8 +22,17 @@
 
 package net.solarnetwork.node.runtime;
 
-import static net.solarnetwork.service.OptionalService.service;
+import static java.util.Collections.singletonMap;
+import static net.solarnetwork.domain.InstructionStatus.InstructionState.Completed;
+import static net.solarnetwork.domain.InstructionStatus.InstructionState.Declined;
+import static net.solarnetwork.node.reactor.InstructionUtils.createErrorResultParameters;
+import static net.solarnetwork.node.reactor.InstructionUtils.createStatus;
+import static net.solarnetwork.service.OptionalServiceCollection.services;
+import static net.solarnetwork.util.ObjectUtils.requireNonNullArgument;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import org.springframework.expression.ExpressionException;
 import net.solarnetwork.domain.datum.DatumSamplesType;
 import net.solarnetwork.domain.datum.GeneralDatum;
 import net.solarnetwork.node.domain.ExpressionRoot;
@@ -33,8 +42,8 @@ import net.solarnetwork.node.reactor.InstructionStatus;
 import net.solarnetwork.node.service.DatumService;
 import net.solarnetwork.node.service.support.BaseIdentifiable;
 import net.solarnetwork.node.service.support.ExpressionConfig;
-import net.solarnetwork.service.OptionalService;
-import net.solarnetwork.util.ObjectUtils;
+import net.solarnetwork.service.ExpressionService;
+import net.solarnetwork.service.support.ExpressionServiceExpression;
 
 /**
  * {@link InstructionHandler} for executing datum-related expressions.
@@ -56,19 +65,21 @@ public class SimpleDatumExpressionService extends BaseIdentifiable implements In
 	/** The instruction parameter for the expression language to use. */
 	public static final String PARAM_EXPRESSION_LANGUAGE = "lang";
 
-	private final OptionalService<DatumService> datumService;
+	private final DatumService datumService;
 
 	/**
 	 * Constructor.
 	 *
 	 * @param datumService
 	 *        the datum service
+	 * @param objectMapper
+	 *        the object mapper to use
 	 * @throws IllegalArgumentException
 	 *         if any argument is {@literal null}
 	 */
-	public SimpleDatumExpressionService(OptionalService<DatumService> datumService) {
+	public SimpleDatumExpressionService(DatumService datumService) {
 		super();
-		this.datumService = ObjectUtils.requireNonNullArgument(datumService, "datumService");
+		this.datumService = requireNonNullArgument(datumService, "datumService");
 	}
 
 	@Override
@@ -78,24 +89,91 @@ public class SimpleDatumExpressionService extends BaseIdentifiable implements In
 
 	@Override
 	public InstructionStatus processInstruction(Instruction instruction) {
-		// TODO Auto-generated method stub
-		GeneralDatum d = new GeneralDatum(UUID.randomUUID().toString());
-		ExpressionRoot root = new ExpressionRoot(d, null, null, service(datumService));
-
-		String[] expressions = instruction.getAllParameterValues(PARAM_EXPRESSION);
-		String expressionServiceId = instruction.getParameterValue(PARAM_EXPRESSION_LANGUAGE);
-
-		ExpressionConfig[] confs = new ExpressionConfig[expressions.length];
-
-		for ( int i = 0; i < confs.length; i++ ) {
-			ExpressionConfig conf = new ExpressionConfig(UUID.randomUUID().toString(),
-					DatumSamplesType.Status, expressions[i], expressionServiceId);
-			confs[i] = conf;
+		if ( instruction == null || !handlesTopic(instruction.getTopic()) ) {
+			return null;
 		}
 
-		populateExpressionDatumProperties(d, confs, root);
+		final String[] expressions = instruction.getAllParameterValues(PARAM_EXPRESSION);
+		if ( expressions == null || expressions.length < 1 ) {
+			return createStatus(instruction, Declined,
+					createErrorResultParameters("No expression parameters provided.", "SDE.00001"));
+		}
 
-		return null;
+		final String expressionServiceId = instruction.getParameterValue(PARAM_EXPRESSION_LANGUAGE);
+		if ( expressionServiceId == null || expressionServiceId.isEmpty() ) {
+			return createStatus(instruction, Declined, createErrorResultParameters(
+					"No expression language parameter provided.", "SDE.00002"));
+		}
+
+		ExpressionService expressionService = null;
+		Iterable<ExpressionService> services = services(getExpressionServices());
+		if ( services != null ) {
+			for ( ExpressionService s : services ) {
+				if ( expressionServiceId.equals(s.getUid()) ) {
+					expressionService = s;
+					break;
+				}
+			}
+		}
+		if ( expressionService == null ) {
+			return createStatus(instruction, Declined, createErrorResultParameters(
+					"Requested expression language service not available.", "SDE.00003"));
+		}
+
+		GeneralDatum d = new GeneralDatum(UUID.randomUUID().toString());
+
+		ExpressionRoot root = new ExpressionRoot(d, null, null, datumService);
+
+		List<Object> results = new ArrayList<>(expressions.length);
+
+		for ( int i = 0; i < expressions.length; i++ ) {
+			ExpressionConfig config = new ExpressionConfig("e" + i, DatumSamplesType.Status,
+					expressions[i], expressionServiceId);
+
+			final ExpressionServiceExpression expr;
+			try {
+				expr = config.getExpression(services);
+			} catch ( ExpressionException e ) {
+				return createStatus(instruction, Declined,
+						createErrorResultParameters(String.format("Error parsing expression [%s]: %s",
+								config.getExpression(), e.getMessage()), "SDE.00004"));
+			}
+
+			Object exprResult = null;
+			if ( expr != null ) {
+				try {
+					exprResult = expr.getService().evaluateExpression(expr.getExpression(), null, root,
+							null, Object.class);
+					if ( log.isTraceEnabled() ) {
+						log.trace(
+								"Instruction [{}] evaluated datum property [{}] expression `{}` \u2192 {}\n\nExpression root: {}",
+								instruction.getIdentifier(), config.getName(), config.getExpression(),
+								exprResult, root);
+					} else if ( log.isDebugEnabled() ) {
+						log.debug(
+								"Instruction [{}] evaluated datum property [{}] expression `{}` \u2192 {}",
+								instruction.getIdentifier(), config.getName(), config.getExpression(),
+								exprResult);
+					}
+				} catch ( ExpressionException e ) {
+					log.warn(
+							"Error evaluating instruction [{}] datum property [{}] expression `{}`: {}\n\nExpression root: {}",
+							instruction.getIdentifier(), config.getName(), config.getExpression(),
+							e.getMessage(), root);
+					return createStatus(instruction, Declined,
+							createErrorResultParameters(
+									String.format("Error evaluating expression [%s]: %s",
+											config.getExpression(), e.getMessage()),
+									"SDE.00005"));
+				}
+			}
+			if ( exprResult != null ) {
+				results.add(exprResult);
+				d.putSampleValue(config.getDatumPropertyType(), config.getName(), exprResult);
+			}
+		}
+
+		return createStatus(instruction, Completed, singletonMap(PARAM_SERVICE_RESULT, results));
 	}
 
 }
