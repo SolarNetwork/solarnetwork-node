444bc1d3b5612c539a620535e44df80f24e31add
Matt Magoffin <matt@solarnetwork.net>
2024-04-02 14:23:33 +1300

    Fix JavaDoc warning.

--- net.solarnetwork.node.control.modbus/src/net/solarnetwork/node/control/modbus/ModbusControlConfig.java
+++ net.solarnetwork.node.control.modbus/src/net/solarnetwork/node/control/modbus/ModbusControlConfig.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ModbusControlConfig.java - 19/09/2022 5:32:52 pm
- * 
+ *
  * Copyright 2022 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -30,9 +30,9 @@ import net.solarnetwork.node.settings.SettingValueBean;
 
 /**
  * Overall configuration for a Modbus Control.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  * @since 3.1
  */
 public class ModbusControlConfig {
@@ -46,8 +46,15 @@ public class ModbusControlConfig {
 	private final List<ModbusWritePropertyConfig> propertyConfigs = new ArrayList<>(8);
 
 	/**
+	 * Constructor.
+	 */
+	public ModbusControlConfig() {
+		super();
+	}
+
+	/**
 	 * Generate a list of setting values from this instance.
-	 * 
+	 *
 	 * @param providerId
 	 *        the setting provider key to use
 	 * @return the list of setting values, never {@literal null}
@@ -68,7 +75,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Populate a setting as a configuration value, if possible.
-	 * 
+	 *
 	 * @param setting
 	 *        the setting to try to handle
 	 * @return {@literal true} if the setting was handled as a configuration
@@ -148,7 +155,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the instance key.
-	 * 
+	 *
 	 * @return the key
 	 */
 	public String getKey() {
@@ -157,7 +164,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the instance ID.
-	 * 
+	 *
 	 * @param key
 	 *        the key to set
 	 */
@@ -167,7 +174,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the modbus network name.
-	 * 
+	 *
 	 * @return the modbusNetworkName
 	 */
 	public String getModbusNetworkName() {
@@ -176,7 +183,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the modbus network name.
-	 * 
+	 *
 	 * @param modbusNetworkName
 	 *        the modbusNetworkName to set
 	 */
@@ -186,7 +193,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the unit ID.
-	 * 
+	 *
 	 * @return the unitId
 	 */
 	public Integer getUnitId() {
@@ -195,7 +202,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the unit ID.
-	 * 
+	 *
 	 * @param unitId
 	 *        the unitId to set
 	 */
@@ -205,7 +212,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the sample cache milliseconds.
-	 * 
+	 *
 	 * @return the sampleCacheMs
 	 */
 	public Long getSampleCacheMs() {
@@ -214,7 +221,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the sample cache milliseconds.
-	 * 
+	 *
 	 * @param sampleCacheMs
 	 *        the sampleCacheMs to set
 	 */
@@ -224,7 +231,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the word order as a key value.
-	 * 
+	 *
 	 * @return the word order as a key; if {@link #getWordOrder()} is
 	 *         {@literal null} then
 	 *         {@link ModbusWordOrder#MostToLeastSignificant} will be returned
@@ -239,7 +246,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the word order as a key value.
-	 * 
+	 *
 	 * @param key
 	 *        the word order key to set; if {@code key} is not valid then
 	 *        {@link ModbusWordOrder#MostToLeastSignificant} will be set
@@ -256,7 +263,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the word order.
-	 * 
+	 *
 	 * @return the wordOrder
 	 */
 	public ModbusWordOrder getWordOrder() {
@@ -265,7 +272,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Set the word order.
-	 * 
+	 *
 	 * @param wordOrder
 	 *        the wordOrder to set
 	 */
@@ -275,7 +282,7 @@ public class ModbusControlConfig {
 
 	/**
 	 * Get the property configurations.
-	 * 
+	 *
 	 * @return the configurations, never {@literal null}
 	 */
 	public List<ModbusWritePropertyConfig> getPropertyConfigs() {
