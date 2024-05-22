aecebe7e65c6e8c3b3cdfe85b0babf8d511eb4d7
Matt Magoffin <matt@solarnetwork.net>
2024-05-22 13:40:43 +1200

    In support of NODE-266, hide the Plugins page if no PluginService available.

--- net.solarnetwork.node.setup.web/src/net/solarnetwork/node/setup/web/ControllerServiceSupport.java
+++ net.solarnetwork.node.setup.web/src/net/solarnetwork/node/setup/web/ControllerServiceSupport.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ControllerServiceSupport.java - 13/02/2017 10:25:07 AM
- * 
+ *
  * Copyright 2007-2017 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -30,14 +30,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import net.solarnetwork.node.service.IdentityService;
 import net.solarnetwork.node.service.PlatformPackageService;
 import net.solarnetwork.node.service.SystemService;
+import net.solarnetwork.node.setup.PluginService;
 import net.solarnetwork.node.setup.web.support.ServiceAwareController;
 import net.solarnetwork.service.OptionalService;
 
 /**
  * Add global services to all MVC controllers.
- * 
+ *
  * @author matt
- * @version 2.1
+ * @version 2.2
  * @since 1.23
  */
 @ControllerAdvice(annotations = { ServiceAwareController.class })
@@ -51,16 +52,26 @@ public class ControllerServiceSupport {
 
 	/**
 	 * The model attribute name for the {@code PlatformPackageService}.
-	 * 
+	 *
 	 * @since 2.1
 	 */
 	public static final String PLATFORM_PACKAGE_SERVICE_ATTRIBUTE = "platformPackageService";
+
+	/**
+	 * The model attribute name for the {@code PluginService}.
+	 *
+	 * @since 2.2
+	 */
+	public static final String PLUGIN_SERVICE_ATTRIBUTE = "pluginService";
 
 	@Resource(name = "systemService")
 	private OptionalService<SystemService> systemService;
 
 	@Resource(name = "platformPackageService")
 	private OptionalService<PlatformPackageService> platformPackageService;
+
+	@Resource(name = "pluginService")
+	private OptionalService<PluginService> pluginService;
 
 	@Autowired
 	private IdentityService identityService;
@@ -74,7 +85,7 @@ public class ControllerServiceSupport {
 
 	/**
 	 * Get the system service.
-	 * 
+	 *
 	 * @return the service
 	 */
 	@ModelAttribute(value = SYSTEM_SERVICE_ATTRIBUTE)
@@ -84,7 +95,7 @@ public class ControllerServiceSupport {
 
 	/**
 	 * The {@link IdentityService}.
-	 * 
+	 *
 	 * @return the service
 	 * @since 1.1
 	 */
@@ -95,13 +106,24 @@ public class ControllerServiceSupport {
 
 	/**
 	 * The {@link PlatformPackageService}.
-	 * 
+	 *
 	 * @return the service
 	 * @since 2.1
 	 */
 	@ModelAttribute(value = PLATFORM_PACKAGE_SERVICE_ATTRIBUTE)
 	public PlatformPackageService platformPackageService() {
 		return service(platformPackageService);
+	}
+
+	/**
+	 * The {@link PlatformPackageService}.
+	 *
+	 * @return the service
+	 * @since 2.2
+	 */
+	@ModelAttribute(value = PLUGIN_SERVICE_ATTRIBUTE)
+	public PluginService pluginService() {
+		return service(pluginService);
 	}
 
 }
