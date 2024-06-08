e083892b0df4bb3a82d173aeb487fba54a6e8447
Matt Magoffin <matt@solarnetwork.net>
2024-06-08 16:44:23 +1200

    NODE-270: support downsample by time in Downsample Filter.

--- net.solarnetwork.node.datum.filter.standard.test/src/net/solarnetwork/node/datum/filter/test/DownsampleDatumFilterServiceTests.java
+++ net.solarnetwork.node.datum.filter.standard.test/src/net/solarnetwork/node/datum/filter/test/DownsampleDatumFilterServiceTests.java
@@ -1,39 +1,51 @@
 /* ==================================================================
  * DownsampleDatumFilterService.java - 24/08/2020 4:42:55 PM
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
 
 package net.solarnetwork.node.datum.filter.test;
 
+import static java.util.Collections.emptyMap;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.easymock.EasyMock;
 import org.junit.Test;
+import net.solarnetwork.domain.datum.AggregateDatumSamples;
 import net.solarnetwork.domain.datum.DatumSamplesOperations;
 import net.solarnetwork.domain.datum.DatumSamplesType;
 import net.solarnetwork.node.datum.filter.std.DownsampleDatumFilterService;
@@ -42,9 +54,9 @@ import net.solarnetwork.node.service.OperationalModesService;
 
 /**
  * Test cases for the {@link DownsampleDatumFilterService}.
- * 
+ *
  * @author matt
- * @version 1.1
+ * @version 1.2
  */
 public class DownsampleDatumFilterServiceTests {
 
@@ -204,6 +216,66 @@ public class DownsampleDatumFilterServiceTests {
 		assertThat("No change because required operational mode not active", out,
 				sameInstance(d.getSamples()));
 		verify(opModesService);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void duration_invalid() {
+		// GIVEN
+		DownsampleDatumFilterService xs = new DownsampleDatumFilterService();
+
+		// WHEN
+		xs.setSampleDuration(Duration.ofNanos(333L));
+
+	}
+
+	@Test
+	public void duration_1s() {
+		// GIVEN
+		DownsampleDatumFilterService xs = new DownsampleDatumFilterService();
+		xs.setSampleDurationSecs(1);
+
+		Clock tick = Clock.tick(Clock.systemUTC(), Duration.ofSeconds(1L));
+
+		Map<Instant, List<DatumSamplesOperations>> input = new LinkedHashMap<>(8);
+		List<DatumSamplesOperations> output = new ArrayList<>(8);
+
+		// go for 3 samples
+		Instant end = Instant.now().plusMillis(3500L);
+
+		// WHEN
+		int i = 0;
+		while ( Instant.now().isBefore(end) ) {
+			SimpleDatum d = SimpleDatum.nodeDatum(TEST_SOURCE);
+			d.getSamples().putInstantaneousSampleValue(TEST_PROP, ++i);
+			DatumSamplesOperations out = xs.filter(d, d.getSamples(), emptyMap());
+			input.computeIfAbsent(tick.instant(), k -> new ArrayList<>(256)).add(d);
+			if ( out != null ) {
+				output.add(out);
+			}
+			try {
+				Thread.sleep(100);
+			} catch ( InterruptedException e ) {
+				// ignore
+			}
+		}
+
+		// THEN
+		assertThat("Output has 1 sample for each passed input time slot", output,
+				hasSize(input.size() - 1));
+		int outIdx = 0;
+		for ( Entry<Instant, List<DatumSamplesOperations>> e : input.entrySet() ) {
+			AggregateDatumSamples expectedAgg = new AggregateDatumSamples();
+			for ( DatumSamplesOperations d : e.getValue() ) {
+				expectedAgg.addSample(d);
+			}
+			DatumSamplesOperations expected = expectedAgg.average(xs.getDecimalScale(),
+					xs.getMinPropertyFormat(), xs.getMaxPropertyFormat());
+			DatumSamplesOperations out = output.get(outIdx++);
+			assertThat("Averaged by duration " + (outIdx + 1), out, is(equalTo(expected)));
+			if ( outIdx >= output.size() ) {
+				break;
+			}
+		}
 	}
 
 }
--- net.solarnetwork.node.datum.filter.standard/src/net/solarnetwork/node/datum/filter/std/DownsampleDatumFilterService.java
+++ net.solarnetwork.node.datum.filter.standard/src/net/solarnetwork/node/datum/filter/std/DownsampleDatumFilterService.java
@@ -1,27 +1,30 @@
 /* ==================================================================
  * DownsampleDatumFilterService.java - 24/08/2020 3:47:12 PM
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
 
 package net.solarnetwork.node.datum.filter.std;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -40,26 +43,29 @@ import net.solarnetwork.settings.support.BasicTitleSettingSpecifier;
 /**
  * Samples transform service that accumulates "sub-sample" values and then
  * produces a down-sampled average with min/max added.
- * 
+ *
  * <p>
- * This filter operates in two different modes: one when a {@code sampleCount}
- * value is configured and another otherwise. When {@code sampleCount} is
- * configured (and greater than {@literal 1}), then the filter will collect
- * {@code sampleCount} samples before generating the down-sampled output
- * samples.
+ * This filter operates in two different modes: one when a
+ * {@code sampleDuration} or {@code sampleCount} value is configured and another
+ * otherwise. When {@code sampleDuration} is configured then the filter will
+ * collect samples within time slots of this duration before generating the
+ * down-sampled output samples. When {@code sampleCount} is configured (and
+ * greater than {@literal 1}), then the filter will collect {@code sampleCount}
+ * samples before generating the down-sampled output samples.
  * </p>
  *
  * <p>
- * When {@code sampleCount} is <b>not</b> configured, then sub-samples are
- * signaled by passing the {@link #SUB_SAMPLE_PROP} key in the {@code parameter}
- * map passed to {@link #filter(Datum, DatumSamplesOperations, Map)}. When
- * invoked in this way the method will always return {@literal null}. Then when
- * a down-sampled output value is needed call
+ * When {@code sampleDuration} and {@code sampleCount} are <b>not</b>
+ * configured, then sub-samples are signaled by passing the
+ * {@link #SUB_SAMPLE_PROP} key in the {@code parameter} map passed to
+ * {@link #filter(Datum, DatumSamplesOperations, Map)}. When invoked in this way
+ * the method will always return {@literal null}. Then when a down-sampled
+ * output value is needed call
  * {@link #filter(Datum, DatumSamplesOperations, Map)} again but without the
  * {@link #SUB_SAMPLE_PROP} key. Then a computed value derived from the
  * collected sub-samples will be returned:
  * </p>
- * 
+ *
  * <ul>
  * <li>Each instantaneous sample property will be transformed into a simple
  * average</li>
@@ -73,9 +79,9 @@ import net.solarnetwork.settings.support.BasicTitleSettingSpecifier;
  * <li>Each status sample property will be transformed into the last seen
  * value.</li>
  * </ul>
- * 
+ *
  * @author matt
- * @version 1.1
+ * @version 1.2
  * @since 2.0
  */
 public class DownsampleDatumFilterService extends DatumFilterSupport
@@ -102,7 +108,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * The {@code sampleCount} property default value.
-	 * 
+	 *
 	 * @since 1.1
 	 */
 	public static Integer DEFAULT_SAMPLE_COUNT = 5;
@@ -114,6 +120,33 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 	private String minPropertyFormat = DEFAULT_MIN_FORMAT;
 	private String maxPropertyFormat = DEFAULT_MAX_FORMAT;
 	private Integer sampleCount = DEFAULT_SAMPLE_COUNT;
+	private Duration sampleDuration;
+
+	private Clock sampleClock;
+
+	/**
+	 * Constructor.
+	 */
+	public DownsampleDatumFilterService() {
+		this(Clock.systemUTC());
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param sampleClock
+	 *        the sample clock to use; if {@literal null} then the system UTC
+	 *        clock will be used
+	 * @since 1.2
+	 */
+	public DownsampleDatumFilterService(Clock sampleClock) {
+		super();
+		this.sampleClock = (sampleClock != null ? sampleClock : Clock.systemUTC());
+	}
+
+	private AggregateDatumSamples newAggregate(String key) {
+		return new AggregateDatumSamples(sampleClock.instant());
+	}
 
 	@Override
 	public DatumSamplesOperations filter(Datum datum, DatumSamplesOperations samples,
@@ -128,20 +161,50 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 			return samples;
 		}
 		final int count = (sampleCount != null ? sampleCount.intValue() : 0);
+		final Duration dur = (sampleDuration != null ? sampleDuration : null);
 		final boolean sub = (parameters != null && parameters.containsKey(SUB_SAMPLE_PROP));
 		AggregateDatumSamples agg = subSamplesBySource.computeIfAbsent(datum.getSourceId(),
-				k -> new AggregateDatumSamples());
+				this::newAggregate);
+		DatumSamplesOperations out = null;
 		synchronized ( agg ) {
-			agg.addSample(samples);
-			if ( (count < 1 && !sub) || (count > 1 && agg.addedSampleCount() >= count) ) {
-				DatumSamples out = agg.average(decimalScale, minPropertyFormat, maxPropertyFormat);
-				subSamplesBySource.remove(datum.getSourceId(), agg);
-				incrementStats(start, samples, out);
-				return out;
+			if ( dur != null ) {
+				Instant datumTs = (datum.getTimestamp() != null ? datum.getTimestamp() : Instant.now());
+				Instant nextTimeSlotStart = agg.getTimestamp().plusNanos(dur.toNanos());
+				if ( datumTs.isAfter(nextTimeSlotStart) ) {
+					if ( agg.addedSampleCount() > 0 ) {
+						out = generateAggregateSample(datum, samples, start, agg);
+					} else {
+						// just move to next time slot, then add sample
+						agg.setTimestamp(sampleClock.instant());
+					}
+				}
 			}
+			if ( out == null ) {
+				agg.addSample(samples);
+			}
+			if ( dur == null
+					&& ((count < 1 && !sub) || (count > 1 && agg.addedSampleCount() >= count)) ) {
+				return generateAggregateSample(datum, samples, start, agg);
+			}
+		}
+		if ( out != null ) {
+			AggregateDatumSamples next = subSamplesBySource.computeIfAbsent(datum.getSourceId(),
+					this::newAggregate);
+			synchronized ( next ) {
+				next.addSample(samples);
+			}
+			return out;
 		}
 		incrementStats(start, samples, null);
 		return null;
+	}
+
+	private DatumSamplesOperations generateAggregateSample(Datum datum, DatumSamplesOperations samples,
+			final long start, AggregateDatumSamples agg) {
+		subSamplesBySource.remove(datum.getSourceId(), agg);
+		DatumSamples out = agg.average(decimalScale, minPropertyFormat, maxPropertyFormat);
+		incrementStats(start, samples, out);
+		return out;
 	}
 
 	@Override
@@ -157,6 +220,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 		results.add(0, new BasicTitleSettingSpecifier("status", statusValue()));
 		populateBaseSampleTransformSupportSettings(results);
 		results.add(new BasicTextFieldSettingSpecifier("sampleCount", DEFAULT_SAMPLE_COUNT.toString()));
+		results.add(new BasicTextFieldSettingSpecifier("sampleDurationSecs", null));
 		results.add(new BasicTextFieldSettingSpecifier("decimalScale",
 				String.valueOf(DEFAULT_DECIMAL_SCALE)));
 		results.add(new BasicTextFieldSettingSpecifier("minPropertyFormat", DEFAULT_MIN_FORMAT));
@@ -167,7 +231,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Get the status value.
-	 * 
+	 *
 	 * @return the status value
 	 */
 	public String statusValue() {
@@ -186,7 +250,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Get the decimal scale.
-	 * 
+	 *
 	 * @return the scale
 	 */
 	public int getDecimalScale() {
@@ -195,11 +259,11 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Set the decimal scale.
-	 * 
+	 *
 	 * <p>
 	 * This is the maximum decimal scale to round averaged results to.
 	 * </p>
-	 * 
+	 *
 	 * @param decimalScale
 	 *        the scale to set; if less than {@literal 0} then {@literal 0} will
 	 *        be used
@@ -213,7 +277,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Get the property name format for "minimum" computed values.
-	 * 
+	 *
 	 * @return the format; defaults to {@link #DEFAULT_MIN_FORMAT}
 	 * @since 1.1
 	 */
@@ -223,11 +287,11 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Set the property name format for "minimum" computed values.
-	 * 
+	 *
 	 * <p>
 	 * This accepts a standard {@link String#format(String, Object...)} style
 	 * formatting template.
-	 * 
+	 *
 	 * @param minPropertyFormat
 	 *        the format to set
 	 * @since 1.1
@@ -238,7 +302,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Get the property name format for "maximum" computed values.
-	 * 
+	 *
 	 * @return the format; defaults to {@link #DEFAULT_MIN_FORMAT}
 	 * @since 1.1
 	 */
@@ -248,11 +312,11 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Set the property name format for "maximum" computed values.
-	 * 
+	 *
 	 * <p>
 	 * This accepts a standard {@link String#format(String, Object...)} style
 	 * formatting template.
-	 * 
+	 *
 	 * @param maxPropertyFormat
 	 *        the format to set
 	 * @since 1.1
@@ -263,7 +327,7 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Get the sample count.
-	 * 
+	 *
 	 * @return the count, or {@literal null}
 	 */
 	public Integer getSampleCount() {
@@ -272,12 +336,76 @@ public class DownsampleDatumFilterService extends DatumFilterSupport
 
 	/**
 	 * Set the sample count.
-	 * 
+	 *
+	 * <p>
+	 * If a {@code sampleDuration} is configured, this value will be ignored.
+	 * </p>
+	 *
 	 * @param sampleCount
 	 *        the count to set, or {@literal null}
 	 */
 	public void setSampleCount(Integer sampleCount) {
 		this.sampleCount = sampleCount;
+	}
+
+	/**
+	 * Get the sample duration.
+	 *
+	 * @return the sample duration
+	 * @since 1.2
+	 */
+	public Duration getSampleDuration() {
+		return sampleDuration;
+	}
+
+	/**
+	 * Set the sample duration.
+	 *
+	 * <p>
+	 * If configured, this takes priority over any configured
+	 * {@code sampleCount}.
+	 * </p>
+	 *
+	 * @param sampleDuration
+	 *        the sample duration to set; must evenly divide evenly into seconds
+	 * @throws IllegalArgumentException
+	 *         if the duration is invalid
+	 * @since 1.2
+	 */
+	public void setSampleDuration(Duration sampleDuration) {
+		this.sampleDuration = sampleDuration;
+		if ( sampleDuration != null ) {
+			sampleClock = Clock.tick(Clock.systemUTC(), sampleDuration);
+		} else {
+			sampleClock = Clock.systemUTC();
+		}
+	}
+
+	/**
+	 * Get the sample duration, as seconds.
+	 *
+	 * @return the sample duration, as seconds, or {@literal null} if no
+	 *         duration configured
+	 * @since 1.2
+	 */
+	public Integer getSampleDurationSecs() {
+		final Duration d = getSampleDuration();
+		return (d != null ? (int) d.getSeconds() : null);
+	}
+
+	/**
+	 * Set the sample duration, as seconds.
+	 *
+	 * @param secs
+	 *        the seconds to set as the sample duration
+	 * @throws IllegalArgumentException
+	 *         if the duration is not valid; see
+	 *         {@link #setSampleDuration(Duration)}
+	 * @since 1.2
+	 */
+	public void setSampleDurationSecs(Integer secs) {
+		Duration d = (secs != null ? Duration.ofSeconds(secs.longValue()) : null);
+		setSampleDuration(d);
 	}
 
 }
