5d0f0408ed82bbe881b07b6e7fa37ef193d820db
Matt Magoffin <matt@solarnetwork.net>
2024-07-08 16:59:52 +1200

    Support DatumSourceIdProvider API.

--- net.solarnetwork.node.datum.carrel.dts/src/net/solarnetwork/node/datum/carrel/dts/DTSConsumptionDatumDataSource.java
+++ net.solarnetwork.node.datum.carrel.dts/src/net/solarnetwork/node/datum/carrel/dts/DTSConsumptionDatumDataSource.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * DTSConsumptionDatumDataSource.java - Mar 28, 2014 6:58:24 AM
- * 
+ *
  * Copyright 2007-2014 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -25,6 +25,8 @@ package net.solarnetwork.node.datum.carrel.dts;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import net.solarnetwork.domain.datum.DatumSamples;
@@ -43,12 +45,12 @@ import net.solarnetwork.settings.support.BasicTextFieldSettingSpecifier;
 /**
  * {@link DatumDataSource} implementation for {@link AcEnergyDatum} with the DTS
  * series watt meter.
- * 
+ *
  * <p>
  * The DTS series watt-hour meter supports the following serial port
  * configuration:
  * </p>
- * 
+ *
  * <ul>
  * <li><b>Baud</b> - 9600</li>
  * <li><b>Mode</b> - RTU</li>
@@ -56,9 +58,9 @@ import net.solarnetwork.settings.support.BasicTextFieldSettingSpecifier;
  * <li><b>Parity</b> - None</li>
  * <li><b>Stop bit</b> - 1</li>
  * </ul>
- * 
+ *
  * @author matt
- * @version 2.1
+ * @version 2.2
  */
 public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSupport
 		implements DatumDataSource, SettingSpecifierProvider {
@@ -69,8 +71,11 @@ public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSu
 	/** The {@code sourceId} default value. */
 	public static final String DEFAULT_SOURCE_ID = "DTS Meter";
 
-	private String sourceId = DEFAULT_SOURCE_ID;
+	private String sourceId;
 
+	/**
+	 * Constructor.
+	 */
 	public DTSConsumptionDatumDataSource() {
 		super();
 		setDisplayName("DTS Series Meter");
@@ -111,6 +116,13 @@ public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSu
 		}
 	}
 
+	@Override
+	public Collection<String> publishedSourceIds() {
+		final String sourceId = resolvePlaceholders(getSourceId());
+		return (sourceId == null || sourceId.isEmpty() ? Collections.emptySet()
+				: Collections.singleton(sourceId));
+	}
+
 	// SettingSpecifierProvider
 
 	@Override
@@ -122,9 +134,9 @@ public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSu
 	public List<SettingSpecifier> getSettingSpecifiers() {
 		List<SettingSpecifier> results = new ArrayList<SettingSpecifier>(10);
 		results.addAll(getIdentifiableSettingSpecifiers());
-		results.add(new BasicTextFieldSettingSpecifier("sourceId", DEFAULT_SOURCE_ID));
-		results.add(new BasicTextFieldSettingSpecifier("modbusNetwork.propertyFilters['uid']",
-				"Modbus Port"));
+		results.add(new BasicTextFieldSettingSpecifier("sourceId", null));
+		results.add(new BasicTextFieldSettingSpecifier("modbusNetwork.propertyFilters['uid']", null,
+				false, "(objectClass=net.solarnetwork.node.io.modbus.ModbusNetwork)"));
 		results.add(new BasicTextFieldSettingSpecifier("unitId", String.valueOf(DEFAULT_UNIT_ID)));
 
 		return results;
@@ -132,7 +144,7 @@ public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSu
 
 	/**
 	 * Get the configured source ID.
-	 * 
+	 *
 	 * @return the source ID
 	 */
 	public String getSourceId() {
@@ -141,7 +153,7 @@ public class DTSConsumptionDatumDataSource extends ModbusDeviceDatumDataSourceSu
 
 	/**
 	 * Set the {@code sourceId} to assign to captured datum.
-	 * 
+	 *
 	 * @param sourceId
 	 *        the source ID to use
 	 */
