77ea75b741321635ee4b6dd4bb1c1910ccad0497
Matt Magoffin <matt@solarnetwork.net>
2024-07-17 05:37:35 +1200

    Add expression test.

--- net.solarnetwork.node.metrics.test/src/net/solarnetwork/node/metrics/service/test/MetricHarvesterDatumFilterServiceTests.java
+++ net.solarnetwork.node.metrics.test/src/net/solarnetwork/node/metrics/service/test/MetricHarvesterDatumFilterServiceTests.java
@@ -23,20 +23,25 @@
 package net.solarnetwork.node.metrics.service.test;
 
 import static java.util.Collections.emptyMap;
+import static java.util.Collections.singleton;
 import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.expect;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.sameInstance;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import org.easymock.Capture;
+import org.easymock.CaptureType;
 import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import net.solarnetwork.common.expr.spel.SpelExpressionService;
 import net.solarnetwork.domain.datum.DatumSamplesOperations;
 import net.solarnetwork.domain.datum.DatumSamplesType;
 import net.solarnetwork.node.domain.datum.SimpleDatum;
@@ -44,6 +49,9 @@ import net.solarnetwork.node.metrics.dao.MetricDao;
 import net.solarnetwork.node.metrics.domain.Metric;
 import net.solarnetwork.node.metrics.service.MetricHarvesterDatumFilterService;
 import net.solarnetwork.node.metrics.service.MetricHarvesterPropertyConfig;
+import net.solarnetwork.node.service.support.ExpressionConfig;
+import net.solarnetwork.service.ExpressionService;
+import net.solarnetwork.service.StaticOptionalServiceCollection;
 
 /**
  * Test cases for the {@link MetricHarvesterDatumFilterService} class.
@@ -57,14 +65,19 @@ public class MetricHarvesterDatumFilterServiceTests {
 	private static final String PROP_1 = "watts";
 
 	private MetricDao metricDao;
+	private ExpressionService exprService;
 	private MetricHarvesterDatumFilterService service;
 
 	@Before
 	public void setup() {
 		metricDao = EasyMock.createMock(MetricDao.class);
+
+		exprService = new SpelExpressionService();
+
 		service = new MetricHarvesterDatumFilterService(metricDao);
-		service.setUid("Test Unchanged");
+		service.setUid("Test Metric Harvester");
 		service.setSourceId("^S");
+		service.setExpressionServices(new StaticOptionalServiceCollection<>(singleton(exprService)));
 	}
 
 	private void replayAll() {
@@ -114,4 +127,50 @@ public class MetricHarvesterDatumFilterServiceTests {
 				equalTo(d.getSampleDouble(DatumSamplesType.Instantaneous, pConfig.getPropertyKey()))));
 	}
 
+	@Test
+	public void expressions() throws Exception {
+		// GIVEN
+		ExpressionConfig config1 = new ExpressionConfig();
+		config1.setName("m1");
+		config1.setExpressionServiceId(exprService.getUid());
+		config1.setExpression("voltage * 2");
+
+		ExpressionConfig config2 = new ExpressionConfig();
+		config2.setName("m2");
+		config2.setExpressionServiceId(exprService.getUid());
+		config2.setExpression("voltage * amps");
+		service.setExpressionConfigs(new ExpressionConfig[] { config1, config2 });
+
+		Capture<Metric> metricCaptor = Capture.newInstance(CaptureType.ALL);
+		expect(metricDao.save(capture(metricCaptor))).andAnswer(() -> {
+			List<Metric> metrics = metricCaptor.getValues();
+			return metrics.get(metrics.size() - 1).getId();
+		}).times(2);
+
+		// WHEN
+		replayAll();
+		Instant start = Instant.now().truncatedTo(ChronoUnit.SECONDS);
+		SimpleDatum d = createTestDatum(start, SOURCE_ID_1, PROP_1, 1);
+		d.putSampleValue(DatumSamplesType.Instantaneous, "amps", 12);
+		d.putSampleValue(DatumSamplesType.Instantaneous, "voltage", 345);
+		DatumSamplesOperations result = service.filter(d, d.getSamples(), emptyMap());
+
+		// THEN
+		assertThat("Input samples returned", result, is(sameInstance(d.getSamples())));
+
+		List<Metric> metrics = metricCaptor.getValues();
+		assertThat("Two metrics persisted", metrics, hasSize(2));
+
+		for ( int i = 0; i < metrics.size(); i++ ) {
+			Metric m = metrics.get(i);
+
+			assertThat("Metric timestamp is datum timestamp", m.getTimestamp(),
+					is(equalTo(d.getTimestamp())));
+			assertThat("Metric type is 'sample'", m.getType(), is(equalTo(Metric.METRIC_TYPE_SAMPLE)));
+			assertThat("Metric name is from prop config", m.getName(),
+					is(equalTo(service.getExpressionConfigs()[i].getPropertyKey())));
+			assertThat("Metric value is from expression result", m.getValue(),
+					is(equalTo(i == 0 ? 690.0 : 4140.0)));
+		}
+	}
 }
