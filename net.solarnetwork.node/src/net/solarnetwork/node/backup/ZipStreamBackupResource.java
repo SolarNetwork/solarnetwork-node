ef66a9183966c053196a4448530d0579e7bd2feb
Matt Magoffin <matt@solarnetwork.net>
2025-06-02 08:59:19 +1200

    Fix deprecation on finalize() method use.

--- net.solarnetwork.node/src/net/solarnetwork/node/backup/ZipStreamBackupResource.java
+++ net.solarnetwork.node/src/net/solarnetwork/node/backup/ZipStreamBackupResource.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ZipStreamBackupResource.java - 2/11/2016 11:02:04 AM
- * 
+ *
  * Copyright 2007-2016 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -30,17 +30,20 @@ import java.io.FileOutputStream;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.Cleaner;
 import java.util.zip.ZipEntry;
 import org.apache.commons.io.input.TeeInputStream;
 
 /**
  * A zip input stream backup resource.
- * 
+ *
  * @author matt
- * @version 1.2
+ * @version 1.3
  * @since 1.46
  */
 public class ZipStreamBackupResource implements BackupResource {
+
+	private static final Cleaner cleaner = Cleaner.create();
 
 	private final InputStream stream;
 	private final ZipEntry entry;
@@ -51,7 +54,7 @@ public class ZipStreamBackupResource implements BackupResource {
 
 	/**
 	 * Construct with values.
-	 * 
+	 *
 	 * @param stream
 	 *        the zip archive stream
 	 * @param entry
@@ -69,6 +72,22 @@ public class ZipStreamBackupResource implements BackupResource {
 		this.path = path;
 	}
 
+	private static final class TempFileCleaner implements Runnable {
+
+		private final File file;
+
+		private TempFileCleaner(File file) {
+			super();
+			this.file = file;
+		}
+
+		@Override
+		public void run() {
+			file.delete();
+		}
+
+	}
+
 	@Override
 	public String getProviderKey() {
 		return providerKey;
@@ -82,11 +101,12 @@ public class ZipStreamBackupResource implements BackupResource {
 	@Override
 	public InputStream getInputStream() throws IOException {
 		// to support calling getInputStream() more than once, tee the input to a temp file
-		// the first time, and subsequent times 
+		// the first time, and subsequent times
 		if ( tempFile != null ) {
 			return new BufferedInputStream(new FileInputStream(tempFile));
 		}
 		tempFile = File.createTempFile(entry.getName(), ".tmp");
+		cleaner.register(this, new TempFileCleaner(tempFile));
 		final BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(tempFile));
 		return new TeeInputStream(new FilterInputStream(stream) {
 
@@ -101,14 +121,6 @@ public class ZipStreamBackupResource implements BackupResource {
 	@Override
 	public long getModificationDate() {
 		return entry.getTime();
-	}
-
-	@Override
-	protected void finalize() throws Throwable {
-		if ( tempFile != null ) {
-			tempFile.delete();
-		}
-		super.finalize();
 	}
 
 	@Override
