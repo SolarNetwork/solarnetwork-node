f34e1c684a7c141b3d506854522a885b7dbebf5d
Matt Magoffin <matt@solarnetwork.net>
2025-06-07 06:35:51 +1200

    Updates for new transactional test setup.

--- net.solarnetwork.node.reactor.dao.jdbc.test/src/net/solarnetwork/node/dao/jdbc/reactor/test/JdbcInstructionDaoTests.java
+++ net.solarnetwork.node.reactor.dao.jdbc.test/src/net/solarnetwork/node/dao/jdbc/reactor/test/JdbcInstructionDaoTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * JdbcInstructionDaoTests.java - Oct 1, 2011 11:55:54 AM
- * 
+ *
  * Copyright 2007 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -49,10 +49,10 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.util.FileCopyUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.solarnetwork.codec.JsonUtils;
@@ -67,7 +67,7 @@ import net.solarnetwork.node.test.AbstractNodeTransactionalTest;
 
 /**
  * Test case for the {@link JdbcInstructionDao} class.
- * 
+ *
  * @author matt
  * @version 2.2
  */
@@ -83,7 +83,7 @@ public class JdbcInstructionDaoTests extends AbstractNodeTransactionalTest {
 
 	private Instruction lastDatum;
 
-	@Before
+	@BeforeTransaction
 	public void setUp() throws Exception {
 		DatabaseSetup setup = new DatabaseSetup();
 		setup.setDataSource(dataSource);
