729c93e0491cad60a3dcd70b85e8ddff286f6684
Matt Magoffin <matt@solarnetwork.net>
2025-06-07 15:38:19 +1200

    Change logic to look for existing DataSource service, not the Configuration for the factory, to work with single Postgres factory Configuration with multi-valued db system property.

--- net.solarnetwork.node.dao.jdbc.con/src/net/solarnetwork/node/dao/jdbc/con/DefaultDataSourceConfigurer.java
+++ net.solarnetwork.node.dao.jdbc.con/src/net/solarnetwork/node/dao/jdbc/con/DefaultDataSourceConfigurer.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * DefaultDataSourceConfigurer.java - 11/06/2021 10:49:53 AM
- * 
+ *
  * Copyright 2021 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -24,9 +24,13 @@ package net.solarnetwork.node.dao.jdbc.con;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Properties;
+import javax.sql.DataSource;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
@@ -35,9 +39,9 @@ import org.slf4j.LoggerFactory;
 /**
  * Publish a data source managed service factory configuration, if one does not
  * already exist.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class DefaultDataSourceConfigurer {
 
@@ -49,6 +53,7 @@ public class DefaultDataSourceConfigurer {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
+	private final BundleContext bundleContext;
 	private final ConfigurationAdmin configurationAdmin;
 	private final Class<?> propertiesClass;
 	private final String propertiesPath;
@@ -56,18 +61,23 @@ public class DefaultDataSourceConfigurer {
 
 	/**
 	 * Constructor.
-	 * 
+	 *
+	 * @param bundleContext
+	 *        the bundle context
 	 * @param configurationAdmin
 	 *        the configuration admin
 	 */
-	public DefaultDataSourceConfigurer(ConfigurationAdmin configurationAdmin) {
-		this(configurationAdmin, DefaultDataSourceConfigurer.class, DEFAULT_PROPERTIES_PATH,
-				DEFAULT_DB_SERVICE_NAME);
+	public DefaultDataSourceConfigurer(BundleContext bundleContext,
+			ConfigurationAdmin configurationAdmin) {
+		this(bundleContext, configurationAdmin, DefaultDataSourceConfigurer.class,
+				DEFAULT_PROPERTIES_PATH, DEFAULT_DB_SERVICE_NAME);
 	}
 
 	/**
 	 * Constructor.
-	 * 
+	 *
+	 * @param bundleContext
+	 *        the bundle context
 	 * @param configurationAdmin
 	 *        the configuration admin
 	 * @param propertiesClass
@@ -77,9 +87,11 @@ public class DefaultDataSourceConfigurer {
 	 * @param dbServiceName
 	 *        the {@literal db} service property name
 	 */
-	public DefaultDataSourceConfigurer(ConfigurationAdmin configurationAdmin, Class<?> propertiesClass,
-			String propertiesPath, String dbServiceName) {
+	public DefaultDataSourceConfigurer(BundleContext bundleContext,
+			ConfigurationAdmin configurationAdmin, Class<?> propertiesClass, String propertiesPath,
+			String dbServiceName) {
 		super();
+		this.bundleContext = bundleContext;
 		this.configurationAdmin = configurationAdmin;
 		this.propertiesClass = propertiesClass;
 		this.propertiesPath = propertiesPath;
@@ -103,8 +115,7 @@ public class DefaultDataSourceConfigurer {
 			return;
 		}
 		try {
-			Configuration cfg = findExistingConfiguration(factoryPid);
-			if ( cfg == null ) {
+			if ( !hasExistingDataSource() ) {
 				createConfiguration(factoryPid, props);
 			}
 		} catch ( IOException | InvalidSyntaxException e ) {
@@ -122,16 +133,11 @@ public class DefaultDataSourceConfigurer {
 		cfg.update((Hashtable) props); // making assumption Properties has only String keys here
 	}
 
-	private Configuration findExistingConfiguration(String pid)
-			throws IOException, InvalidSyntaxException {
-		String filter = "(&(" + ConfigurationAdmin.SERVICE_FACTORYPID + "=" + pid
-				+ ")(serviceProperty.db=" + dbServiceName + "))";
-		Configuration[] configurations = configurationAdmin.listConfigurations(filter);
-		if ( configurations != null && configurations.length > 0 ) {
-			return configurations[0];
-		} else {
-			return null;
-		}
+	private boolean hasExistingDataSource() throws IOException, InvalidSyntaxException {
+		String filter = "(db=" + dbServiceName + ")";
+		Collection<ServiceReference<DataSource>> refs = bundleContext
+				.getServiceReferences(DataSource.class, filter);
+		return !refs.isEmpty();
 	}
 
 }
